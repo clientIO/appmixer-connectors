dc1fd7e207c4c188e392de0c099d76d29bec3790
vladimir talas <vladimir.talas@gmail.com>
2024-01-17 13:52:04 +0100

    Hubspot update

--- src/appmixer/hubspot/crm/GetContact/GetContact.js
+++ src/appmixer/hubspot/crm/GetContact/GetContact.js
@@ -7,22 +7,38 @@ module.exports = {
 
         const {
             email,
-            contactId
+            contactId,
+            properties,
+            associations
         } = context.messages.in.content;
 
         const { auth } = context;
         const hs = new Hubspot(auth.accessToken, context.config);
-        const emailQuery = email ? '&idProperty=email' : '';
+        const params = {
+            properties: properties || '',
+            associations: associations || [],
+            archived: false
+        };
+
+        if (email) {
+            params.idProperty = 'email';
+        }
 
         try {
+            const url = `crm/v3/objects/contacts/${contactId || encodeURIComponent(email)}`;
+            console.log(url);
             const { data } = await hs.call(
-                'get', `crm/v3/objects/contacts/${contactId || encodeURIComponent(email)}`, {}, { query: 'archived=false'.concat(emailQuery) });
+                'get',
+                url,
+                params
+            );
 
             return context.sendJson(data, 'contact');
 
         } catch (err) {
+
             if (err.status || (err.response && err.response.status) === 404) {
-                return context.sendJson({  email, contactId  }, 'notFound');
+                return context.sendJson({ email, contactId }, 'notFound');
             }
             throw err;
         }
