6dbf80b8e8f2101f833990f2e66a2f080b75fd55
harshaio <114140359+harshaio@users.noreply.github.com>
2024-03-13 22:08:48 +0530

    remove isValidType check (#42)

--- src/appmixer/utils/controls/SetVariable/SetVariable.js
+++ src/appmixer/utils/controls/SetVariable/SetVariable.js
@@ -1,28 +1,5 @@
 'use strict';
 
-const isValidType = (value, expectedType) => {
-    switch (expectedType) {
-        case 'number':
-            // Check for both type 'number' and not NaN
-            return typeof value === 'number' && !isNaN(value);
-        case 'text':
-        case 'textarea':
-        case 'filepicker':
-            // Check for type 'string'
-            return typeof value === 'string';
-        case 'date-time':
-            // Check for a valid date; Date.parse returns NaN for invalid dates
-            return typeof value === 'string' && !isNaN(Date.parse(value));
-        case 'toggle':
-            // Check for boolean type
-            return typeof value === 'boolean';
-        default:
-            // Unexpected type, could log this or handle as needed
-            return false;
-    }
-};
-
-
 /**
  * OnStart component reacts to the 'start' message only. It triggers when the flow starts.
  * @extends {Component}
@@ -49,9 +26,6 @@ module.exports = {
 
             // type can be number, text, textarea, filepicker, date and toggle
             const { name, type, ...value } = variable;
-            if (!isValidType(value[type], type)) {
-                throw new context.CancelError(`Type mismatch for variable "${name}". Expected type: ${type}.`);
-            }
             outputObject[name] = value[type];
         });
         // Send the output
