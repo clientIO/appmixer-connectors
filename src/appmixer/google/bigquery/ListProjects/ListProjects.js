4d0ef29ad3c9ef39aaf551c34cd533f1a383f167
vladimir talas <vladimir.talas@gmail.com>
2024-03-22 17:06:41 +0100

    google.Bigquery: remove cloud scopes

--- src/appmixer/google/bigquery/ListProjects/ListProjects.js
+++ src/appmixer/google/bigquery/ListProjects/ListProjects.js
@@ -6,7 +6,7 @@ module.exports = {
     async receive(context) {
 
         const authClient = commons.getAuthLibraryOAuth2Client(context.auth);
-        const res = await authClient.request({ url: 'https://cloudresourcemanager.googleapis.com/v1/projects' });
+        const res = await authClient.request({ url: 'https://bigquery.googleapis.com/bigquery/v2/projects' });
         const { projects } = res.data;
 
         return context.sendJson({ projects }, 'out');
