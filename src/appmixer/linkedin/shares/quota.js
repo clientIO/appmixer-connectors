c24d6449622e9a64448f2f86a322a7164b0e5bd7
harshaio <114140359+harshaio@users.noreply.github.com>
2024-03-28 18:46:33 +0530

    LinkedIn: Upgrade to the most recent API version #63

--- src/appmixer/linkedin/shares/quota.js
+++ src/appmixer/linkedin/shares/quota.js
@@ -1,7 +1,7 @@
 'use strict';
 
 // Note: Daily quotas refresh at midnight PST.
-let getStartOfNextWindow = function() {
+const getStartOfNextWindow = () => {
 
     return moment.utc().startOf('day').add(1, 'day').add(8, 'hours').valueOf();
 };
@@ -12,7 +12,7 @@ module.exports = {
         // https://developer.linkedin.com/docs/share-on-linkedin
         // According to LinkedIn the limits are 25 per day for one user
         {
-            limit: 25,                     // the quota is 25 per 1 day
+            limit: 150,                     // the quota is 150 per 1 day
             window: 1000 * 60 * 60 * 24,          // 1 day
             throttling: 'window-sliding',
             queueing: 'fifo',
@@ -20,9 +20,9 @@ module.exports = {
             scope: 'userId'
         },
         // https://developer.linkedin.com/docs/share-on-linkedin
-        // According to LinkedIn the limits are 125000 per day for application
+        // According to LinkedIn the limits are 100000 per day for application
         {
-            limit: 125000,
+            limit: 100000,
             throttling: {
                 type: 'window-fixed',
                 getStartOfNextWindow: getStartOfNextWindow
