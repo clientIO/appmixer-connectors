d5c1f8f1969d33463fe1a9a52691e6dc8e8a1f19
Jiří Hofman <jiri.hofman@gmail.com>
2024-04-19 12:40:24 +0200

    JIRA: Fix labels in inspector for CreateIssue and UpdateIssue

--- src/appmixer/jira/issues/ListField/ListField.js
+++ src/appmixer/jira/issues/ListField/ListField.js
@@ -13,6 +13,14 @@ module.exports = {
             endpoint = `${apiUrl}user/search?query=`;
         }
 
+        // Fix labels endpoint. The one provided by Get create issue metadata is not working.
+        if (endpoint.includes('rest/api/1.0/labels/suggest')) {
+            endpoint = `${apiUrl}label`;
+            const response = await commons.get(endpoint, auth);
+
+            return context.sendJson(response?.values, 'out');
+        }
+
         const response = await commons.get(endpoint, auth);
         return context.sendJson(response, 'out');
     },
@@ -20,6 +28,16 @@ module.exports = {
     fieldToSelectArray(response) {
 
         if (Array.isArray(response)) {
+
+            // If response is an array of strings, eg. for labels
+            if (typeof response[0] === 'string') {
+                return response.map(res => {
+                    return {
+                        label: res,
+                        value: res
+                    };
+                });
+            }
 
             const filtered = response.filter(res => {
                 if (res.emailAddress) {
