bfb83505c3254904228d504c6fe0ca74b002064b
harshaio <harsha@client.io>
2024-04-30 14:21:49 +0530

    update quota

--- src/appmixer/utils/csv/quota.js
+++ src/appmixer/utils/csv/quota.js
@@ -4,17 +4,10 @@ module.exports = {
 
     rules: [
         {
-            limit: 100,
-            throttling: 'limit-concurrency',
-            queueing: 'fifo',
-            resource: 'loadFiles'
-        },
-        {
             limit: 5,
             throttling: 'limit-concurrency',
             queueing: 'fifo',
-            resource: 'loadFiles',
-            scope: 'userId'
+            resource: 'loadFiles'
         }
     ]
 };
