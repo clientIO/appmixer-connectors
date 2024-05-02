022e9c63f5a67fd00b31a78b82a459097ee1d403
harshaio <114140359+harshaio@users.noreply.github.com>
2024-05-02 13:10:56 +0530

    utils.csv.AddRows (#86)
    * addRows component
    
    * update csv.AddRows
    
    * add test flow
    
    * update
    
    * add quota per user
    
    * update quota
    
    * update

---
+++ src/appmixer/utils/csv/AddRows/AddRows.js
@@ -0,0 +1,44 @@
+'use strict';
+const CSVProcessor = require('../CSVProcessor');
+
+module.exports = {
+
+    async receive(context) {
+
+        const {
+            fileId,
+            delimiter = ',',
+            rows,
+            parseNumbers,
+            parseBooleans
+        } = context.messages.in.content;
+
+        // check if it withHeaders or not(if rows is an array of array string or array of objects)
+        const withHeaders = !Array.isArray(rows[0]);
+
+        const processor = new CSVProcessor(context, fileId, {
+            withHeaders,
+            delimiter,
+            parseNumbers,
+            parseBooleans
+        });
+        let rowsArray = rows;
+        if (typeof rows === 'string') {
+            // Try to parse string as JSON.
+            try {
+                rowsArray = JSON.parse(rows);
+            } catch (error) {
+                throw new context.CancelError(
+                    'Property \'rows\' should be array or well formed JSON array string. ' +
+                    'In case of CSV string, use modifier \'Split\' to create an Array.',
+                    error
+                );
+            }
+        }
+        const savedFile = await processor.addRows({ rows: rowsArray }, (idx, currentRow, isEndOfFile) => {
+            return isEndOfFile;
+        });
+
+        return context.sendJson({ fileId: savedFile.fileId }, 'fileId');
+    }
+};
