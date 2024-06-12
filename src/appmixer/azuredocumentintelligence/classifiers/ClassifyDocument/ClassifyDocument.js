70f1d12b272981fac57d34f300272698c43fded5
Jiří Hofman <jiri.hofman@gmail.com>
2024-06-12 11:16:47 +0200

    - fix azure lib version - all input options for ClassifyDocument

--- src/appmixer/azuredocumentintelligence/classifiers/ClassifyDocument/ClassifyDocument.js
+++ src/appmixer/azuredocumentintelligence/classifiers/ClassifyDocument/ClassifyDocument.js
@@ -8,23 +8,41 @@ module.exports = {
     async receive(context) {
 
         const { endpoint, apiKey } = context.config;
-        const { classifierId } = context.messages.in.content;
+        const { classifierId, fileId, fileUrl, base64Source } = context.messages.in.content;
 
         const client = DocumentIntelligence(endpoint, { key: apiKey });
+
+        let options;
+
+        if (fileId) {
+            // Using "Classify Document From Stream" operation from "Document Classifiers" API
+            const fileInfo = await context.getFileInfo(fileId);
+            const fileStream = await context.getFileReadStream(fileId);
+            options = {
+                contentType: fileInfo.contentType || 'application/octet-stream',
+                body: fileStream
+            };
+        } else {
+            // Using "Classify Document" operation from "Document Classifiers" API
+            if (fileUrl) {
+                options = { body: { urlSource: fileUrl } };
+            } else {
+                options = { body: { base64Source } };
+            }
+        }
+
+        await context.log({ step: 'Classifying document', endpoint, classifierId, fileId, fileUrl, base64Source });
         const initialResponse = await client
             .path('/documentClassifiers/{classifierId}:analyze', classifierId)
-            .post({
-                contentType: 'application/json',
-                body: {
-                    urlSource: `https://d15f34w2p8l1cc.cloudfront.net/hearthstone/50241b011e3fb5ec0314535ff6e132faf69772f57590dc4c99570fce8d4ab79d.png`,
-                }
-            });
+            .post(options);
 
         if (isUnexpected(initialResponse)) {
             throw initialResponse.body.error;
         }
 
         const poller = await getLongRunningPoller(client, initialResponse);
+        await context.log({ step: 'Polling for result', result: initialResponse.body });
+
         const analyzeResult = (await poller.pollUntilDone()).body.analyzeResult;
 
         await context.sendJson(analyzeResult, 'out');
