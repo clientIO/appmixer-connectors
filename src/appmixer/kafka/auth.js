dbbcdbb26ca5af9736afb95657cb5c08810642ca
David Durman <daviddurman@gmail.com>
2024-07-04 15:13:00 +0200

    Improve tooltips in Kafka connector

--- src/appmixer/kafka/auth.js
+++ src/appmixer/kafka/auth.js
@@ -18,22 +18,22 @@ module.exports = {
                 clientId: {
                     type: 'text',
                     name: 'Client ID',
-                    tooltip: 'A logical identifier of an application.'
+                    tooltip: 'A logical identifier of an application. Choose a meaningful name to logically group your clients. Example: booking-events-processor.'
                 },
                 brokers: {
                     type: 'text',
                     name: 'Brokers',
-                    tooltip: 'Comma separated broker addresses.'
+                    tooltip: 'A comma separated list of broker addresses. Example: glider.srvs.cloudkafka.com:9094.'
                 },
                 ssl: {
                     type: 'text',
                     name: 'SSL',
-                    tooltip: 'Enable/Disable SSL.'
+                    tooltip: 'Enable/Disable SSL. Enter the string "TRUE" if you want to enable SSL.'
                 },
                 saslMechanism: {
                     type: 'text',
                     name: 'SASL Mechanism',
-                    tooltip: 'SASL mechanism type.'
+                    tooltip: 'SASL mechanism type. Supported values: SCRAM-SHA-256, SCRAM-SHA-512, PLAIN.'
                 },
                 saslUsername: {
                     type: 'text',
