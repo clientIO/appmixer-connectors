935ba8f7e04c3ab113e034f2c1d175c9c38d0487
David Durman <daviddurman@gmail.com>
2024-07-17 12:46:57 +0200

    Add GetFileMetadata component

---
+++ src/appmixer/google/drive/GetFileMetadata/GetFileMetadata.js
@@ -0,0 +1,22 @@
+'use strict';
+const { google } = require('googleapis');
+const commons = require('../drive-commons');
+
+module.exports = {
+
+    async receive(context) {
+
+        const auth = commons.getOauth2Client(context.auth);
+        const drive = google.drive({ version: 'v3', auth });
+        const {
+            fileId
+        } = context.messages.in.content;
+
+        const { data: metadata } = await drive.files.get({
+            fileId: fileId === 'string' ? fileId : fileId.id,
+            fields: '*'
+        });
+
+        return context.sendJson({ googleDriveFileMetadata: metadata }, 'out');
+    }
+};
