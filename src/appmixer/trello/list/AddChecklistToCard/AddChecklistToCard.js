cd1659ddd0b93edcbd6f50c81fa1a1392d490d61
Jiří Hofman <jiri.hofman@gmail.com>
2024-07-22 11:43:53 +0200

    AddChecklistToCard, AddItemToChecklist, ListCardChecklist

--- src/appmixer/trello/list/AddChecklistToCard/AddChecklistToCard.js
+++ src/appmixer/trello/list/AddChecklistToCard/AddChecklistToCard.js
@@ -5,7 +5,7 @@ module.exports = {
 
     async receive(context) {
 
-        const { checklistName, boardListCardId } = context.messages.in.content;
+        const { checklistName, checklistItems, boardListCardId } = context.messages.in.content;
 
         const url = '/1/cards/' + boardListCardId + '/checklists';
 
@@ -17,6 +17,29 @@ module.exports = {
                 name: checklistName.trim()
             }
         });
+
+        // Add checklist items to the checklist
+        if (checklistItems) {
+            const items = checklistItems.split('\n');
+
+            if (items.length > 10) {
+                throw new context.CancelError('Maximum 10 checklist items are allowed');
+            }
+
+            for (let i = 0; i < items.length; i++) {
+                const item = items[i];
+                await context.httpRequest({
+                    headers: { 'Content-Type': 'application/json' },
+                    method: 'POST',
+                    url: `https://api.trello.com/1/checklists/${data.id}/checkItems?${commons.getAuthQueryParams(context)}`,
+                    data: {
+                        'name': item.trim()
+                    }
+                });
+            }
+
+            data.items = items;
+        }
 
         const checklist = {
             ...data,
---
+++ src/appmixer/trello/list/ListCardChecklists/ListCardChecklists.js
@@ -0,0 +1,72 @@
+'use strict';
+const commons = require('../../trello-commons');
+
+module.exports = {
+
+    async receive(context) {
+
+        const generateOutputPortOptions = context.properties.generateOutputPortOptions;
+        const { boardListCardId, outputType, isSource } = context.messages.in.content;
+
+        if (generateOutputPortOptions) {
+            return this.getOutputPortOptions(context, outputType);
+        }
+
+        if (isSource) {
+            if (!boardListCardId) {
+                return context.sendJson({ boardListCardId: [] }, 'out');
+            }
+        }
+
+        const { data } = await context.httpRequest({
+            headers: { 'Content-Type': 'application/json' },
+            url: `https://api.trello.com/1/cards/${boardListCardId}/checklists?${commons.getAuthQueryParams(context)}`
+        });
+
+
+        return commons.sendArrayOutput({
+            context,
+            outputPortName: 'out',
+            outputType,
+            records: data
+        });
+    },
+
+    getOutputPortOptions(context, outputType) {
+
+        if (outputType === 'item') {
+            return context.sendJson(
+                [
+                    { label: 'ID', value: 'id' },
+                    { label: 'Board ID', value: 'idBoard' },
+                    { label: 'Name', value: 'name' }
+                ],
+                'out'
+            );
+        } else if (outputType === 'items') {
+            return context.sendJson(
+                [
+                    {
+                        label: 'Checklists',
+                        value: 'items',
+                        schema: {
+                            type: 'array',
+                            items: {
+                                type: 'object',
+                                properties: {
+                                    'id': { title: 'ID', type: 'string' },
+                                    'idBoard': { title: 'Board ID', type: 'string' },
+                                    'name': { title: 'Name', type: 'string' }
+                                }
+                            }
+                        }
+                    }
+                ],
+                'out'
+            );
+        } else {
+            // file
+            return context.sendJson([{ label: 'File ID', value: 'fileId' }], 'out');
+        }
+    }
+};
