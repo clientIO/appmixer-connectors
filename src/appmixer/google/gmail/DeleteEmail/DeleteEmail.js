6cfcc0da46c0cd6c4718180451326fed51ce4e12
Sayam Nasir <sayam@client.io>
2024-07-23 17:28:56 +0500

    es-lint2

--- src/appmixer/google/gmail/DeleteEmail/DeleteEmail.js
+++ src/appmixer/google/gmail/DeleteEmail/DeleteEmail.js
@@ -5,17 +5,17 @@ module.exports = {
     async receive(context) {
         const { emailId, permanently } = context.messages.in.content;
         const endpoint = permanently
-        ? `/users/me/messages/${emailId}`
-        : `/users/me/messages/${emailId}/trash`;
+            ? `/users/me/messages/${emailId}`
+            : `/users/me/messages/${emailId}/trash`;
         const options = {
             method: permanently ? 'DELETE' : 'POST',
             headers: { 'Content-Type': 'application/json' }
         };
         const result = await commons.callEndpoint(context, endpoint, options);
-        if (result.data == '') {
+        if (result.data === '') {
             return context.sendJson({}, 'deleted');
+        } else {
+            return context.sendJson(result.data, 'out');
         }
-        else
-        return context.sendJson(result.data, 'out');
     }
 };
