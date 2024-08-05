f3c7e7a62ee327b0274c0f2623141bb5a4077ecd
Jiří Hofman <jiri.hofman@gmail.com>
2024-08-05 23:01:02 +0200

    Add lock

--- src/appmixer/system/core/OnAnyFlowComponentError/OnAnyFlowComponentError.js
+++ src/appmixer/system/core/OnAnyFlowComponentError/OnAnyFlowComponentError.js
@@ -4,59 +4,78 @@ module.exports = {
 
     async start(context) {
 
-        // Get current WEBHOOK_FLOW_COMPONENT_ERROR from system config
-        const config = await context.callAppmixer({
-            endPoint: '/config',
-            method: 'GET'
-        });
-        const urls = config.find(c => c.key === 'WEBHOOK_FLOW_COMPONENT_ERROR')?.value?.split(',') || [];
-        // Use enqueueOnly to automatically enqueue the webhook in the `receive` method.
-        const newUrl = context.getWebhookUrl({ enqueueOnly: true });
-        const updatedUrls = [...urls, newUrl].join(',');
+        let lock;
+        try {
+            // Lock common for all instances of this component. There can be only one process updating the config.
+            lock = await context.lock('OnAnyFlowComponentError');
 
-        await context.log({ step: 'Update config', newUrls: updatedUrls, oldUrls: urls });
-        // Update WEBHOOK_FLOW_COMPONENT_ERROR in system config
-        await context.callAppmixer({
-            endPoint: '/config',
-            method: 'POST',
-            body: {
-                key: 'WEBHOOK_FLOW_COMPONENT_ERROR',
-                value: updatedUrls
+            // Get current WEBHOOK_FLOW_COMPONENT_ERROR from system config
+            const config = await context.callAppmixer({
+                endPoint: '/config',
+                method: 'GET'
+            });
+            const urls = config.find(c => c.key === 'WEBHOOK_FLOW_COMPONENT_ERROR')?.value?.split(',') || [];
+            // Use enqueueOnly to automatically enqueue the webhook in the `receive` method.
+            const newUrl = context.getWebhookUrl({ enqueueOnly: true });
+            const updatedUrls = [...urls, newUrl].join(',');
+
+            await context.log({ step: 'Update config', newUrls: updatedUrls, oldUrls: urls });
+            // Update WEBHOOK_FLOW_COMPONENT_ERROR in system config
+            await context.callAppmixer({
+                endPoint: '/config',
+                method: 'POST',
+                body: {
+                    key: 'WEBHOOK_FLOW_COMPONENT_ERROR',
+                    value: updatedUrls
+                }
+            });
+        } finally {
+            if (lock) {
+                lock.unlock();
             }
-        });
+        }
     },
 
     async stop(context) {
 
-        // Get current WEBHOOK_FLOW_COMPONENT_ERROR from system config
-        const config = await context.callAppmixer({
-            endPoint: '/config',
-            method: 'GET'
-        });
-        const urls = config.find(c => c.key === 'WEBHOOK_FLOW_COMPONENT_ERROR')?.value?.split(',');
-        const newUrl = context.getWebhookUrl();
-        const updatedUrls = urls.filter(url => url !== newUrl).join(',');
+        try {
+            // Lock common for all instances of this component. There can be only one process updating the config.
+            lock = await context.lock('OnAnyFlowComponentError');
 
-        if (!updatedUrls) {
-            await context.log({ step: 'Remove config' });
-            // No more URLs, remove the config
-            await context.callAppmixer({
-                endPoint: '/config/WEBHOOK_FLOW_COMPONENT_ERROR',
-                method: 'DELETE'
+            // Get current WEBHOOK_FLOW_COMPONENT_ERROR from system config
+            const config = await context.callAppmixer({
+                endPoint: '/config',
+                method: 'GET'
             });
-            return;
-        }
+            const urls = config.find(c => c.key === 'WEBHOOK_FLOW_COMPONENT_ERROR')?.value?.split(',');
+            const newUrl = context.getWebhookUrl({ enqueueOnly: true });
+            const updatedUrls = urls.filter(url => url !== newUrl).join(',');
 
-        await context.log({ step: 'Update config', urls: updatedUrls });
-        // Update WEBHOOK_FLOW_COMPONENT_ERROR in system config
-        await context.callAppmixer({
-            endPoint: '/config',
-            method: 'POST',
-            body: {
-                key: 'WEBHOOK_FLOW_COMPONENT_ERROR',
-                value: updatedUrls
+            if (!updatedUrls) {
+                await context.log({ step: 'Remove config' });
+                // No more URLs, remove the config
+                await context.callAppmixer({
+                    endPoint: '/config/WEBHOOK_FLOW_COMPONENT_ERROR',
+                    method: 'DELETE'
+                });
+                return;
             }
-        });
+
+            await context.log({ step: 'Update config', urls: updatedUrls });
+            // Update WEBHOOK_FLOW_COMPONENT_ERROR in system config
+            await context.callAppmixer({
+                endPoint: '/config',
+                method: 'POST',
+                body: {
+                    key: 'WEBHOOK_FLOW_COMPONENT_ERROR',
+                    value: updatedUrls
+                }
+            });
+        } finally {
+            if (lock) {
+                lock.unlock();
+            }
+        }
     },
 
     async receive(context) {
