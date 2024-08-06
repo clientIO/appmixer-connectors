36b7e476901a7da7aff1b0ba2fb29b72cfebd886
David Durman <daviddurman@gmail.com>
2024-08-06 15:28:53 +0200

    google.drive.CreateShortcut

---
+++ src/appmixer/google/drive/CreateShortcut/CreateShortcut.js
@@ -0,0 +1,45 @@
+'use strict';
+const mime = require('mime-types');
+const { google } = require('googleapis');
+const commons = require('../drive-commons');
+
+module.exports = {
+
+    async receive(context) {
+
+        const auth = commons.getOauth2Client(context.auth);
+        const drive = google.drive({ version: 'v3', auth });
+        const { userId } = context.auth;
+        let { fileId, shortcutName, destinationFolder } = context.messages.in.content;
+
+
+        let normalizedDestinationFolder;
+        if (destinationFolder) {
+            normalizedDestinationFolder = typeof destinationFolder === 'string' ? destinationFolder : destinationFolder.id;
+        }
+        const normalizedFileId = typeof fileId === 'string' ? fileId : fileId.id;
+
+        const resource = {
+            name: shortcutName,
+            mimeType: 'application/vnd.google-apps.shortcut',
+            shortcutDetails: {
+                targetId: normalizedFileId
+            }
+        };
+
+        if (destinationFolder) {
+            resource.parents = [normalizedDestinationFolder];
+        }
+
+        const response = await drive.files.create({
+            quotaUser: userId,
+            resource,
+            fields: '*'
+        });
+
+        return context.sendJson({
+            googleDriveFileId: normalizedFileId,
+            googleDriveFileMetadata: response.data
+        }, 'out');
+    }
+};
