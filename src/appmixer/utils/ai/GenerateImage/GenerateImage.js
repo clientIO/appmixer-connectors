dca6c8e97284da25df360d7c6e2c72159a839bca
David Durman <daviddurman@gmail.com>
2024-08-09 16:27:13 +0200

    utils.ai: SendPrompt, GenerateImage

---
+++ src/appmixer/utils/ai/GenerateImage/GenerateImage.js
@@ -0,0 +1,41 @@
+'use strict';
+
+module.exports = {
+
+    receive: async function(context) {
+
+        const { prompt, size } = context.messages.in.content;
+        const apiKey = context.config.apiKey;
+
+        if (!apiKey) {
+            return new context.CancelError('Missing \"apiKey\" system setting of the appmixer.utils.ai module pointing to OpenAI. Please provide it in the Connector Configuration section of the Appmixer Backoffice.');
+        }
+
+        const url = `https://api.openai.com/v1/images/generations`;
+        const { data } = await context.httpRequest.post(url, {
+            model: 'dall-e-3',
+            prompt,
+            size,
+            n: 1
+        }, {
+            headers: {
+                'Authorization': `Bearer ${apiKey}`,
+                'Content-Type': 'application/json'
+            }
+        });
+
+        let imageUrl;
+        if (data && data.data && data.data.length > 0) {
+            imageUrl = data.data[0].url;
+        }
+
+        if (imageUrl) {
+            const response = await context.httpRequest.get(imageUrl, { responseType: 'stream' });
+            const readStream = response.data;
+            const filename = `generated-image-${new Date}.png`;
+            const contentType = response.headers['content-type'] || 'application/octet-stream';
+            const file = await context.saveFileStream(filename, readStream);
+            return context.sendJson({ fileId: file.fileId, prompt, size }, 'out');
+        }
+    }
+};
---
+++ src/appmixer/utils/ai/SendPrompt/SendPrompt.js
@@ -0,0 +1,44 @@
+'use strict';
+
+module.exports = {
+
+    receive: async function(context) {
+
+        const { prompt } = context.messages.in.content;
+        const apiKey = context.config.apiKey;
+
+        if (!apiKey) {
+            return new context.CancelError('Missing \"apiKey\" system setting of the appmixer.utils.ai module pointing to OpenAI. Please provide it in the Connector Configuration section of the Appmixer Backoffice.');
+        }
+
+        const url = `https://api.openai.com/v1/chat/completions`;
+        const { data } = await context.httpRequest.post(url, {
+            model: 'gpt-4o',
+            messages: [
+                {
+                    role: 'system',
+                    content: 'You are a helpful assistant.',
+                    name: 'system'
+                },
+                {
+                    role: 'user',
+                    content: prompt,
+                    name: 'user'
+                }
+            ]
+        }, {
+            headers: {
+                'Authorization': `Bearer ${apiKey}`,
+                'Content-Type': 'application/json'
+            }
+        });
+
+        let answer = '';
+
+        if (data && data.choices) {
+            answer = data.choices[0].message.content;
+        }
+
+        return context.sendJson({ answer }, 'out');
+    }
+};
