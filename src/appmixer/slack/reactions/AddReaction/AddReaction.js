a93ff47a0419aec959521625eb9ca25363c2cea8
Jiří Hofman <jiri.hofman@gmail.com>
2024-08-21 14:11:02 +0200

    add scopes for adding and removing reactions to be able to list all channels add function to list all channels

--- src/appmixer/slack/reactions/AddReaction/AddReaction.js
+++ src/appmixer/slack/reactions/AddReaction/AddReaction.js
@@ -6,7 +6,11 @@ module.exports = {
 
     async receive(context) {
 
-        const { channel, timestamp, name } = context.messages.in.content;
+        const { channel, timestamp, name, isSource } = context.messages.in.content;
+        if (isSource) {
+            await listChannels(context);
+            return;
+        }
         // Initialize Slack Web API client
         const web = new WebClient(context.auth.accessToken);
         const result = await web.reactions.add({
@@ -18,3 +22,27 @@ module.exports = {
         return context.sendJson(result, 'out');
     }
 };
+
+/**
+ * List all channels: private, public, direct messages, and group messages.
+ * Similar to ListChannels.js with channelsToSelectArray function. This also lists direct messages and group messages.
+ */
+async function listChannels(context) {
+
+    const client = new WebClient(context.auth.accessToken);
+    const options = { exclude_archived: 1, types: 'private_channel,public_channel,im,mpim', limit: 1000 };
+
+    try {
+        const channels = await client.conversations.list(options);
+        const myChannels = channels.channels?.filter(channel => channel.is_member).map(channel => {
+            return {
+                label: channel.name,
+                value: channel.id
+            };
+        });
+        context.sendJson(myChannels, 'out');
+    } catch (err) {
+        context.log({ step: 'List channels error', error: err });
+        context.sendJson([], 'out');
+    }
+}
