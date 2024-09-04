00c2b5fab874cb2278ffbc0af3f68ca7599c867d
vladimir talas <vladimir.talas@gmail.com>
2024-09-04 09:54:43 +0200

    update

--- src/appmixer/wiz/core/UploadSecurityScan/UploadSecurityScan.js
+++ src/appmixer/wiz/core/UploadSecurityScan/UploadSecurityScan.js
@@ -102,14 +102,12 @@ async function streamToString(stream) {
     return Buffer.concat(chunks).toString('utf-8');
 }
 
-const uploadFile = async function(context, { url, fileId }) {
-
-    const stream = await context.getFileReadStream(fileId);
+const uploadFile = async function(context, { url, fileContent }) {
 
     const upload = await context.httpRequest({
         method: 'PUT',
         url,
-        data: await streamToString(stream), // stream upload is not implemented on the wiz side
+        data: fileContent, // stream upload is not implemented on the wiz side
         headers: {
             'Content-Type': 'application/json'
         }
@@ -117,18 +115,40 @@ const uploadFile = async function(context, { url, fileId }) {
     context.log({ stage: 'upload finished', uploadData: upload.statusCode });
 };
 
+const getFile = async function(context) {
+
+    const { filename, fileId, fileContent } = context.messages.in.content;
+
+    let json;
+    let name;
+    if (fileId) {
+        const fileInfo = await context.getFileInfo(fileId);
+        const stream = await context.getFileReadStream(fileId);
+        json = await streamToString(stream);
+        name = filename || fileInfo.filename;
+    } else {
+        try {
+            json = JSON.parse(fileContent);
+            name = filename || 'incident-report.json';
+        } catch (e) {
+            throw new context.CancelError('Invalid Input: FileContent', e);
+        }
+    }
+
+    return { content: json, name };
+};
+
 module.exports = {
 
     // docs: https://win.wiz.io/reference/pull-cloud-resources
     async receive(context) {
 
-        const { filename, fileId } = context.messages.in.content;
-        const fileInfo = await context.getFileInfo(fileId);
+        const { name, fileContent } = await getFile(context);
 
-        const { url, systemActivityId } = await requestUpload(context, { filename: filename || fileInfo.filename });
+        const { url, systemActivityId } = await requestUpload(context, { filename: name });
         context.log({ stage: 'requestUpload response ', url, systemActivityId });
 
-        await uploadFile(context, { url, fileId });
+        await uploadFile(context, { url, fileContent });
 
         const status = await getStatus(context, systemActivityId);
         return context.sendJson(status, 'out');
