da5501a7d06d63794acf6f12a834425b64803538
David Durman <daviddurman@gmail.com>
2024-10-10 08:45:34 +0200

    google.drive (major update) (#130)
    * Update tooltips: CopyFile, CreateFolder, DeleteFileOrFolder, DownloadFile, GetFile. Extend DownloadFile to make GetFile and ExportFile obsolete;
    
    * Add GetFileMetadata component
    
    * Improve FindFileOrFolder to return all metadata
    
    * Extend FindFileOrFolder component with options to return all matching results
    
    * ListFiles accepts fileTypes
    
    * MoveFileOrFolder: change file id field to googlepicker; Update tooltips
    
    * CopyFile, DeleteFileOrFolder: change file id field to googlepicker
    
    * fix schema of google drive file ID; use [DOCS, FOLDERS] for google pickers
    
    * NewFile: update tooltips. Return entire google metadata object
    
    * Rename NewFile to NewFileOrFolder and add fileTypesRestriction option
    
    * add UpdatedFile trigger
    
    * google.drive.AddFileOrFolderPermission
    
    * google.drive.CreateFileFromText
    
    * google.drive.CreateShortcut
    
    * remove obsolete of GetFile, ExportFile and ListFiles; Add UpdateFileOrFolder
    
    * final touch
    
    * remove quotaUser; the authenticated user goes against quotas anyway
    
    * fix linting errors
    
    * disable quotes for eslint in FindFilesOrFolders
    
    * remove unused userId variable
    
    * normalize file Id in DeleteFileOrFolder
    
    * fix output port of NewFileOrFolder trigger in sendJson()
    
    * unify outpout ports of UpdatedFileOrFolder
    
    * add Count and Index to FindFilesOrFolders
    
    * fix source for FindFilesOrFolders outpout port options
    
    * move auth.js file to google/drive; Review and fix all OAuth scopes
    
    * remove package-lock.json
    
    * remove obsolete test flows
    
    * rename drive-commons.js to lib.js
    
    * remove unused packages; use auth from appmixer:google:drive

--- src/appmixer/google/drive/DeleteFileOrFolder/DeleteFileOrFolder.js
+++ src/appmixer/google/drive/DeleteFileOrFolder/DeleteFileOrFolder.js
@@ -1,16 +1,18 @@
 'use strict';
 const { google } = require('googleapis');
-const commons = require('../drive-commons');
+const lib = require('../lib');
 
 module.exports = {
 
     async receive(context) {
 
-        const auth = commons.getOauth2Client(context.auth);
+        const auth = lib.getOauth2Client(context.auth);
         const drive = google.drive({ version: 'v3', auth });
         let { fileId } = context.messages.in.content;
 
-        await drive.files.delete({ fileId });
-        return context.sendJson({ fileId }, 'out');
+        const normalizedFileId = typeof fileId === 'string' ? fileId : fileId.id;
+
+        await drive.files.delete({ fileId: normalizedFileId });
+        return context.sendJson({ fileId: normalizedFileId }, 'out');
     }
 };
