d8e22045f85649baebf09fb964c9f0690b5dec1c
Jiří Hofman <jiri.hofman@gmail.com>
2024-11-28 12:40:45 +0100

    CreateRule schema + behaviour

--- src/appmixer/imperva/waf/CreateRule/CreateRule.js
+++ src/appmixer/imperva/waf/CreateRule/CreateRule.js
@@ -6,7 +6,7 @@ module.exports = {
 
     receive: async function(context) {
 
-        const { siteId, name, filter, action, ttl } = context.messages.in.content;
+        const { siteId, ...rule } = context.messages.in.content;
 
         const { data } = await context.httpRequest({
             headers: {
@@ -16,43 +16,9 @@ module.exports = {
             url: `${baseUrl}/v2/sites/${siteId}/rules`,
             method: 'POST',
             data: {
-                name,
-                filter,
-                action,
-                ttl
+                ...rule
             }
         });
-
-        await context.log({ step: 'Rule created', data });
-
-        if (ttl) {
-            const now = new Date();
-            // Fake it to be in 1 min in the future
-            const inTheFuture = new Date(now.getTime() + ttl * 60000);
-            const removeAfter = inTheFuture.getTime();
-            /* Creating record in `serviceState`:
-            {
-                "key" : "imperva-rule-delete-{siteId}-{ruleId}",
-                "service" : "appmixer-imperva",
-                "value" : {removeAfter}
-            }
-            */
-            const serviceStateKey = `imperva-rule-delete-${siteId}-${data.rule_id}`;
-            await context.log({ step: 'Creating a rule with TTL', ttl, removeAfter, serviceStateKey });
-            await context.service.stateSet(serviceStateKey, removeAfter);
-            // Only works in plugins
-            // await context.scheduleJob(`imperva-rule-delete-${data.id}`, cronTimeFromTTL, async () => {
-            //     await context.log('info', '[IMPERVA] Deleting rule with TTL', data.id);
-            //     await context.httpRequest({
-            //         headers: {
-            //             'x-API-Id': context.auth.id,
-            //             'x-API-Key': context.auth.key
-            //         },
-            //         url: `${baseUrl}/v2/sites/${siteId}/rules/${data.id}`,
-            //         method: 'DELETE'
-            //     });
-            // });
-        }
 
         return context.sendJson({ ...data, siteId }, 'out');
     }
