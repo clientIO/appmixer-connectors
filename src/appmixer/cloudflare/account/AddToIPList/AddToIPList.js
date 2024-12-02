303cb575a23f666a8cb4d904fa1c3cc08c45bf20
vladimir talas <vladimir.talas@gmail.com>
2024-12-02 10:54:30 +0100

    update

--- src/appmixer/cloudflare/account/AddToIPList/AddToIPList.js
+++ src/appmixer/cloudflare/account/AddToIPList/AddToIPList.js
@@ -1,16 +1,38 @@
 const ZoneCloudflareClient = require('../../ZoneCloudflareClient');
 const { OUTPUT_PORT } = require('../../lib');
 
-function output(context, message, output) {
-    return context.sendJson({ message }, output.valueOf());
-}
+let attempts = 0;
+const getStatus = async function(context, client, { account, id }) {
+
+    context.log({ stage: 'GETTING STATUS', id, attempts });
+    const { data } = await client.callEndpoint(context, {
+        action: `/accounts/${account}/rules/lists/bulk_operations/${id}`
+    });
+
+    context.log({ stage: 'STATUS DATA', data });
+    if (data?.result?.status === 'failed') {
+        throw new context.CancelError(data?.result?.error || data?.errors);
+    }
+
+    if (data?.result?.status !== 'completed') {
+        attempts++;
+        if (attempts <= 5) {
+            await new Promise(r => setTimeout(r, 2000));
+            return await getStatus(context, client, { account, id });
+        } else {
+            throw new context.CancelError(data.errors);
+        }
+    }
+
+    return data.result;
+};
 
 module.exports = {
     async receive(context) {
 
         const { apiKey, email } = context.auth;
         const { accountsFetch, listFetch } = context.properties;
-        const { account, list } = context.messages.in.content;
+        const { account, list, ips } = context.messages.in.content;
 
         const client = new ZoneCloudflareClient({ email, apiKey });
 
@@ -43,38 +65,24 @@ module.exports = {
             return context.sendJson([], OUTPUT_PORT.SUCCESS);
         }
 
+        const ipsList = ips.AND;
+
         try {
 
-            const items = [
-                {
-                    // asn: ,
-                    ip: '192.168.1.1',
-                    comment: 'sdflkdsjflk '
-                    // hostname: {
-                    //     url_hostname: 'lksajdklfj.com'
-                    // }
-                }
-            ];
             // https://developers.cloudflare.com/api/operations/lists-create-list-items
             const { data } = await client.callEndpoint(context, {
-                action: `/accounts/${account}/rules/lists/${list}`,
-                method: 'GET',
-                data: items
+                action: `/accounts/${account}/rules/lists/${list}/items`,
+                method: 'POST',
+                data: ipsList
             });
-            // const { data } = await client.callEndpoint(context, {
-            //     action: `/accounts/${account}/rules/lists/${list}/items`,
-            //     method: 'POST',
-            //     data: items
-            // });
 
-            console.log('----------------------');
-            console.log(data);
-            return context.sendJson(data, OUTPUT_PORT.SUCCESS);
+            await getStatus(context, client, { id: data.result.operation_id, account });
+            return context.sendJson({ message: `IPs successfully added: ${ipsList.map(item => item.ip).join(',')}` }, OUTPUT_PORT.SUCCESS);
 
         } catch (err) {
 
-            console.log(err.response.data);
-            return context.sendJson({ message: 'error' }, OUTPUT_PORT.FAILURE);
+            context.log({ stage: 'ERROR', detail: err?.response || err });
+            return context.sendJson({ message: `Adding IPs failed: ${ipsList.map(item => item.ip).join(', ')}` }, OUTPUT_PORT.FAILURE);
         }
     }
 };
