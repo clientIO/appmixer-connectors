93d76fe5594de19739245267cb1cc7120a6159b3
Sayam Nasir <sayam@client.io>
2024-12-05 21:25:13 +0500

    improving auth.js

--- src/appmixer/webflow/auth.js
+++ src/appmixer/webflow/auth.js
@@ -12,6 +12,27 @@ module.exports = {
             }
         },
 
+        async requestProfileInfo(context) {
+            const url = 'https://api.webflow.com/v2/token/authorized_by';
+            const headers = {
+                'accept': 'application/json',
+                'authorization': `Bearer ${context.apiKey}`,
+                'accept-version': '2.0.0',
+                'Content-Type': 'application/json'
+            };
+
+            const response = await context.httpRequest({ url, method: 'GET', headers });
+
+            if (response.data && response.data.firstName && response.data.lastName) {
+                const fullName = `${response.data.firstName} ${response.data.lastName}`;
+                return { name: fullName };
+            } else if (response.data && response.data.email) {
+                return { name: response.data.email };
+            }
+        },
+
+        accountNameFromProfileInfo: 'name',
+
         validate: {
             method: 'GET',
             url: 'https://api.webflow.com/v2/sites',
