bc616cce558a0b42e35d4041fe19485f7809b1ec
Sayam Nasir <sayam@client.io>
2024-12-16 16:02:05 +0500

    Revert "updates to ListBuckets S3"
    This reverts commit 50ea7f39a1b3712d50b84836e32dbad28ccec1b0.

--- src/appmixer/aws/s3/ListBuckets/ListBuckets.js
+++ src/appmixer/aws/s3/ListBuckets/ListBuckets.js
@@ -9,64 +9,30 @@ module.exports = {
 
     async receive(context) {
 
-        const sendWholeArray = context.properties?.sendWholeArray || false;
-        const generateOutputPortOptions = context.properties?.generateOutputPortOptions || false;
-
-        if (generateOutputPortOptions) {
-            return this.getOutputPortOptions(context, sendWholeArray);
-        }
+        const { sendWholeArray } = context.properties;
 
         const { s3 } = commons.init(context);
         const { Buckets } = await s3.listBuckets().promise();
 
         if (sendWholeArray) {
-            return context.sendJson({ Buckets }, 'bucket');
+            return context.sendJson(Buckets, 'bucket');
         } else {
-            const promises = Buckets.map(bucket => context.sendJson(bucket, 'bucket'));
+            const promises = [];
+            Buckets.forEach(bucket => {
+                promises.push(context.sendJson(bucket, 'bucket'));
+            });
             return Promise.all(promises);
         }
     },
 
-    bucketsToSelectArray(data) {
-        const buckets = data.Buckets || []; // Extract the array from the object
-        if (Array.isArray(buckets)) {
+    bucketsToSelectArray(buckets) {
+
+        if (buckets && Array.isArray(buckets)) {
             return buckets.map(bucket => ({
                 label: bucket.Name,
                 value: bucket.Name
             }));
         }
         return [];
-    },
-
-    getOutputPortOptions(context, sendWholeArray) {
-
-        if (sendWholeArray === false) {
-            return context.sendJson(
-                [
-                    { label: 'Name', value: 'Name' },
-                    { label: 'CreationDate', value: 'CreationDate' }
-                ],
-                'bucket'
-            );
-        } else {
-            return context.sendJson(
-                [
-                    {
-                        label: 'Buckets', value: 'Buckets',
-                        schema: {
-                            type: 'array',
-                            items: {
-                                type: 'object',
-                                properties: {
-                                    Name: { type: 'string', title: 'Name' },
-                                    CreationDate: { type: 'string', title: 'CreationDate' }
-                                }
-                            }
-                        }
-                    }
-                ],
-                'bucket'
-            );
-        }
     }
 };
