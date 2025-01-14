b18335abd01c8213bbb3a8c56966b1441733dac0
Zbynek Pelunek <zbynekpelunek@gmail.com>
2025-01-14 14:35:12 +0100

    fix for stop sequence array

--- src/appmixer/openai/core/createChatCompletion/createChatCompletion.js
+++ src/appmixer/openai/core/createChatCompletion/createChatCompletion.js
@@ -9,16 +9,13 @@ module.exports = {
             role: content.model.includes('o1') && message.role === 'system' ? 'developer' : message.role,
             content: message.content
         }));
-        context.log({ step: 'transformedMessages', transformedMessages });
-        const transformedStopSequence = content.stop.ADD.map(sequence => {
-            if (Object.keys(sequence).length > 0 && sequence.sequence !== '') {
+        const transformedStopSequence = content.stop.ADD.reduce((acc, sequence) => {
+            if (sequence.sequence) {
                 context.log({ step: 'sequence check', check: `sequence is not null or empty string: ${sequence.sequence}` });
-                return sequence.sequence;
+                acc.push(sequence.sequence);
             }
-        });
-        const filteredStopSequence = transformedStopSequence.filter(s => s !== '');
-        context.log({ step: 'transformedStopSequence', transformedStopSequence });
-        context.log({ step: 'filteredStopSequence', filteredStopSequence });
+            return acc;
+        }, []);
 
         if (transformedStopSequence.length > 4) {
             throw new context.CancelError('Request can have a maximum of 4 stop sequences.');
@@ -27,10 +24,8 @@ module.exports = {
         const requestBody = {
             ...content,
             messages: transformedMessages,
-            stop: filteredStopSequence.length > 0 ? filteredStopSequence : undefined
+            stop: transformedStopSequence.length > 0 ? transformedStopSequence : undefined
         };
-
-        context.log({ step: 'requestBody', requestBody });
 
         const req = {
             url: 'https://api.openai.com/v1/chat/completions',
