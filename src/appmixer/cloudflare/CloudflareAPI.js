2ab3c5d33d75d202ba8b7db064f2daa0dfeba55d
vladimir talas <vladimir.talas@gmail.com>
2025-01-16 13:12:30 +0100

    cleanup

--- src/appmixer/cloudflare/CloudflareAPI.js
+++ src/appmixer/cloudflare/CloudflareAPI.js
@@ -23,17 +23,6 @@ module.exports = class CloudflareAPI {
         };
     }
 
-    async verify(context) {
-
-        const headers = this.getHeaders();
-
-        return context.httpRequest({
-            method: 'GET',
-            url: 'https://api.cloudflare.com/client/v4/user/tokens/verify',
-            headers
-        });
-    }
-
     async verifyGlobalApiKey(context) {
 
         const headers = this.getHeaders();
@@ -61,59 +50,5 @@ module.exports = class CloudflareAPI {
             data,
             params
         });
-    }
-
-    /**
-     * https://developers.cloudflare.com/api/operations/getZoneRuleset
-     */
-    listZoneRulesets(context) {
-
-        return context.httpRequest({
-            method: 'GET',
-            url: `https://api.cloudflare.com/client/v4/zones/${this.zoneId}/rulesets`,
-            headers: this.getHeaders()
-        }).then(resp => resp.data.result);
-    }
-
-    async getRules(context, rulesetId) {
-
-        const response = await context.httpRequest({
-            method: 'GET',
-            url: `https://api.cloudflare.com/client/v4/zones/${this.zoneId}/rulesets/${rulesetId}`,
-            headers: this.getHeaders()
-        });
-
-        return response.data;
-    }
-
-    async createRulesetAndBlockRule(context, ips) {
-
-        const { data } = await context.httpRequest({
-            method: 'POST',
-            url: `https://api.cloudflare.com/client/v4/zones/${this.zoneId}/rulesets`,
-            headers: this.getHeaders(),
-            data: {
-                kind: 'zone',
-                phase: 'http_request_firewall_custom',
-                name: 'Http Request Firewall Custom Ruleset',
-                description: 'Created by Salt Security\'s Cloudflare Integration. Firewall rules of identified attackers will be added to this ruleset.',
-                rules: [this.getBlockRule(1, ips)]
-            }
-        });
-
-        return data;
-    }
-
-    // https://developers.cloudflare.com/api/resources/rulesets/subresources/rules/
-    createBlockRule(context, { rulesetId, rule }) {
-        const url = `https://api.cloudflare.com/client/v4/zones/${this.zoneId}/rulesets/${rulesetId}/rules`;
-        const headers = this.getHeaders();
-        return context.httpRequest.post(url, rule, { headers }).then(resp => resp.data);
-    }
-
-    updateBlockRule(context, rulesetId, rule) {
-        const url = `https://api.cloudflare.com/client/v4/zones/${this.zoneId}/rulesets/${rulesetId}/rules/${rule.id}`;
-        const headers = this.getHeaders();
-        return context.httpRequest.patch(url, rule, { headers }).then(resp => resp.data);
     }
 };
