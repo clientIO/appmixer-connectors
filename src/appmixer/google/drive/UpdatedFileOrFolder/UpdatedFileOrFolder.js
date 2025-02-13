16847acd8fce83e8362046d21f98f58047e17431
vladimir talas <lades@client.io>
2025-02-13 20:21:14 +0100

    google.drive (minor) FindFilesOrFolders now support recursive search (#377)

--- src/appmixer/google/drive/UpdatedFileOrFolder/UpdatedFileOrFolder.js
+++ src/appmixer/google/drive/UpdatedFileOrFolder/UpdatedFileOrFolder.js
@@ -1,113 +1,27 @@
-const moment = require('moment');
-const { google } = require('googleapis');
 const lib = require('../lib');
+const moment = require('moment');
 
-const getUpdatedFiles = async (lock, drive, folder, fileTypesRestriction, pageToken, updatedFiles = []) => {
+function isUpdatedFileOrFolder(change) {
 
-    const { data: { changes, newStartPageToken, nextPageToken } } = await drive.changes.list({
-        pageToken,
-        fields: '*',
-        includeRemoved: false
-    });
+    const file = change.file;
+    if (!file) return false;
 
-    changes.forEach(change => {
-        if (change.changeType === 'file' && !change.removed && !change.file?.trashed && new Date(change.file?.modifiedTime) > new Date(change.file?.createdTime)) {
-
-            if (updatedFiles.find(file => file.id === change.file.id)) {
-                // We've already processed this file (sometimes the same file change may occur multiple times in the change list).
-                return;
-            }
-
-            const mimeType = change.file?.mimeType;
-
-            // Check for location folder match.
-            if (folder && !change.file?.parents?.includes(folder)) {
-                return;
-            }
-
-
-            // Check for file type restrictions.
-            if (fileTypesRestriction && fileTypesRestriction.length) {
-
-                let isAllowed = false;
-                for (let i = 0; i < fileTypesRestriction.length; i++) {
-                    const allowedType = fileTypesRestriction[i];
-                    isAllowed = allowedType === '#FILE' ? false : mimeType.startsWith(allowedType);
-                    if (isAllowed) break; // No need to search further since we found a match.
-                }
-                if (!isAllowed) return;
-            }
-
-            updatedFiles.push(change.file);
-        }
-    });
-
-    if (nextPageToken) {
-        await lock.extend(20000);
-        return getUpdatedFiles(lock, drive, folder, fileTypesRestriction, nextPageToken, updatedFiles);
-    }
-
-    return { updatedFiles, newStartPageToken };
-};
-
-const detectUpdatedFiles = async function(context) {
-
-    const { folder = {}, fileTypesRestriction } = context.properties;
-
-    let lock = null;
-    try {
-        lock = await context.lock(context.componentId, { maxRetryCount: 0 });
-    } catch (err) {
-        await context.stateSet('hasSkippedMessage', true);
-        return;
-    }
-
-    try {
-        const { startPageToken, processedFiles = [] } = await context.loadState();
-        const auth = lib.getOauth2Client(context.auth);
-        const drive = google.drive({ version: 'v3', auth });
-
-        await context.stateSet('hasSkippedMessage', false);
-
-        const {
-            updatedFiles,
-            newStartPageToken
-        } = await getUpdatedFiles(lock, drive, folder.id, fileTypesRestriction, startPageToken);
-
-        const processedFilesSet = lib.processedItemsBuffer(processedFiles);
-
-        for (let file of updatedFiles) {
-            if (!processedFilesSet.has(file.id)) {
-                processedFilesSet.add(newStartPageToken, file.id);
-                const out = {
-                    isFolder: file.mimeType === 'application/vnd.google-apps.folder',
-                    isFile: file.mimeType !== 'application/vnd.google-apps.folder',
-                    googleDriveFileMetadata: file
-                };
-                await context.sendJson(out, 'out');
-                await context.stateSet('processedFiles', processedFilesSet.export());
-            }
-        }
-
-        await context.stateSet('startPageToken', newStartPageToken);
-
-    } finally {
-        if (lock) {
-            await lock.unlock();
-        }
-    }
-};
+    return change.changeType === 'file' &&
+    !change.removed &&  // exclude removed files
+    !file.trashed &&  // exclude trashed files
+    new Date(file.modifiedTime) > new Date(file.createdTime);  // file modified after creation.
+}
 
 module.exports = {
 
     async start(context) {
 
-        return this.registerWebhook(context);
+        return lib.registerWebhook(context);
     },
 
     stop(context) {
 
-        return this.unregisterWebhook(context);
+        return lib.unregisterWebhook(context);
     },
 
     async receive(context) {
@@ -121,7 +35,7 @@ module.exports = {
             return context.response();
         }
 
-        await detectUpdatedFiles(context);
+        await lib.checkMonitoredFiles(context, { filter: isUpdatedFileOrFolder });
 
         return context.response();
     },
@@ -131,78 +45,16 @@ module.exports = {
         const { expiration, hasSkippedMessage } = await context.loadState();
 
         if (hasSkippedMessage) {
-            // a message came when we were processing results,
-            // we have to check for updated files again
-            await detectUpdatedFiles(context);
+            // A message came when we were processing results,
+            // we have to check for changes again.
+            await lib.checkMonitoredFiles(context, { filter: isUpdatedFileOrFolder });
         }
 
         if (expiration) {
             const renewDate = moment(expiration).subtract(5, 'hours');
             if (moment().isSameOrAfter(renewDate)) {
-                return this.registerWebhook(context);
+                return lib.registerWebhook(context);
             }
-        }
-    },
-
-    async registerWebhook(context) {
-
-        let lock = null;
-        try {
-            lock = await context.lock(context.componentId);
-            await this.unregisterWebhook(context);
-        } catch (err) {
-            if (!err.response || err.response.status !== 404) {
-                lock && lock.unlock();
-                throw err;
-            }
-        }
-
-        try {
-            const auth = lib.getOauth2Client(context.auth);
-            const drive = google.drive({ version: 'v3', auth });
-            let pageToken = await context.stateGet('startPageToken');
-
-            if (!pageToken) {
-                // when triggered for the first time, we have to get the startPageToken
-                const { data: token } = await drive.changes.getStartPageToken();
-                pageToken = token.startPageToken;
-            }
-
-            const expiration = moment().add(1, 'day').valueOf();
-            const { data } = await drive.changes.watch({
-                includeRemoved: false,
-                pageToken,
-                requestBody: {
-                    address: context.getWebhookUrl() + '?enqueueOnly=true',
-                    id: context.componentId,
-                    payload: true,
-                    token: context.componentId,
-                    type: 'web_hook',
-                    expiration
-                }
-            });
-
-            await context.stateSet('startPageToken', pageToken);
-            await context.stateSet('webhookId', data.resourceId);
-            await context.stateSet('expiration', expiration);
-        } finally {
-            lock && lock.unlock();
-        }
-    },
-
-    async unregisterWebhook(context) {
-
-        const { webhookId } = await context.loadState();
-        if (webhookId) {
-            const auth = lib.getOauth2Client(context.auth);
-            const drive = google.drive({ version: 'v3', auth });
-
-            return drive.channels.stop({
-                requestBody: {
-                    resourceId: webhookId,
-                    id: context.componentId
-                }
-            });
         }
     }
 };
