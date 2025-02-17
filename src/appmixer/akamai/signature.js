8465bab96b95940b1189d6287eb037b1bf61caca
Zbynek Pelunek <zbynekpelunek@gmail.com>
2025-02-17 10:12:47 +0100

    ActivateList.listId is now text type

--- src/appmixer/akamai/signature.js
+++ src/appmixer/akamai/signature.js
@@ -1,14 +1,13 @@
 const EdgeGrid = require('akamai-edgegrid');
 
 module.exports = {
-    generateAuthorizationHeader({ clientToken, clientSecret, accessToken, hostnameUrl, method, path, body }) {
-        const debug = true;
+    generateAuthorizationHeader({ clientToken, clientSecret, accessToken, hostnameUrl, method, path, body, debug }) {
         const eg = new EdgeGrid(clientToken, clientSecret, accessToken, hostnameUrl, debug);
 
         const auth = eg.auth({
             path,
             method,
-            body: body ?? {}
+            body
         });
 
         return auth.request;
