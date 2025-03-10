bb17602d3c7b3565e1b1f22d4329c111c53bc343
vladimir talas <vladimir.talas@gmail.com>
2025-03-10 21:38:27 +0100

    update

--- src/appmixer/jira/issues/GetIssueTransitions/GetIssueTransitions.js
+++ src/appmixer/jira/issues/GetIssueTransitions/GetIssueTransitions.js
@@ -1,20 +1,65 @@
 'use strict';
 
 const commons = require('../../jira-commons');
+const lib = require('../../lib.outputPortOptions');
+
+const schema = {
+    'id': { 'type': 'string', 'title': 'Id' },
+    'name': { 'type': 'string', 'title': 'Name' },
+    'to': {
+        'type': 'object',
+        'properties': {
+            'self': { 'type': 'string', 'title': 'To.Self' },
+            'description': { 'type': 'string', 'title': 'To.Description' },
+            'iconUrl': { 'type': 'string', 'title': 'To.Icon Url' },
+            'name': { 'type': 'string', 'title': 'To.Name' },
+            'id': { 'type': 'string', 'title': 'To.Id' },
+            'statusCategory': {
+                'type': 'object',
+                'properties': {
+                    'self': { 'type': 'string', 'title': 'To.Status Category.Self' },
+                    'id': { 'type': 'number', 'title': 'To.Status Category.Id' },
+                    'key': { 'type': 'string', 'title': 'To.Status Category.Key' },
+                    'colorName': { 'type': 'string', 'title': 'To.Status Category.Color Name' },
+                    'name': { 'type': 'string', 'title': 'To.Status Category.Name' }
+                },
+                'title': 'To.Status Category'
+            }
+        },
+        'title': 'To'
+    },
+    'hasScreen': { 'type': 'boolean', 'title': 'Has Screen' },
+    'isGlobal': { 'type': 'boolean', 'title': 'Is Global' },
+    'isInitial': { 'type': 'boolean', 'title': 'Is Initial' },
+    'isAvailable': { 'type': 'boolean', 'title': 'Is Available' },
+    'isConditional': { 'type': 'boolean', 'title': 'Is Conditional' },
+    'isLooped': { 'type': 'boolean', 'title': 'Is Looped' }
+};
 
 module.exports = {
 
     async receive(context) {
 
         const { profileInfo, auth } = context;
-        const { id } = context.messages.in.content;
+        const { id, outputType } = context.messages.in.content;
+
+        if (context.properties.generateOutputPortOptions) {
+            return lib.getOutputPortOptions(context, outputType, schema, {
+                label: 'Transitions',
+                value: 'transitions'
+            });
+        }
 
         try {
             const transitions = await commons.get(`${profileInfo.apiUrl}issue/${id}/transitions`, auth);
-            return context.sendJson(transitions, 'out');
+            return lib.sendArrayOutput({
+                context,
+                records: transitions,
+                outputType
+            });
         } catch (e) {
             if (context.properties.variablesFetch) {
-                return context.sendJson([], 'out');
+                return lib.sendArrayOutput({ context, records: transitions, outputType: 'array' });
             }
             throw e;
         }
---
+++ src/appmixer/jira/lib.outputPortOptions.js
@@ -0,0 +1,87 @@
+const pathModule = require('path');
+
+const DEFAULT_PREFIX = 'jira-export';
+
+module.exports = {
+
+    async sendArrayOutput({ context, outputPortName = 'out', outputType = 'array', records = [] }) {
+        if (outputType === 'first') {
+            // One by one.
+            await context.sendJson(records[0], outputPortName);
+        } else if (outputType === 'object') {
+            // One by one.
+            await context.sendArray(records, outputPortName);
+        } else if (outputType === 'array') {
+            // All at once.
+            await context.sendJson({ result: records }, outputPortName);
+        } else if (outputType === 'file') {
+
+            // Into CSV file.
+            const csvString = toCsv(records);
+
+            let buffer = Buffer.from(csvString, 'utf8');
+            const componentName = context.flowDescriptor[context.componentId].label || context.componentId;
+            const fileName = `${context.config.outputFilePrefix || DEFAULT_PREFIX}-${componentName}.csv`;
+            const savedFile = await context.saveFileStream(pathModule.normalize(fileName), buffer);
+
+            await context.log({ step: 'File was saved', fileName, fileId: savedFile.fileId });
+            await context.sendJson({ fileId: savedFile.fileId }, outputPortName);
+        } else {
+            throw new context.CancelError('Unsupported outputType ' + outputType);
+        }
+    },
+
+    getOutputPortOptions(context, outputType, itemSchema, { label, value }) {
+
+        if (outputType === 'object' || outputType === 'first') {
+            const options = Object.keys(itemSchema)
+                .map(field => {
+                    const schema = itemSchema[field];
+                    const label = schema.title;
+                    delete schema.title;
+
+                    return {
+                        label, value: field, schema
+                    };
+                });
+            return context.sendJson(options, 'out');
+        }
+
+        if (outputType === 'array') {
+            return context.sendJson([{
+                label,
+                value,
+                schema: {
+                    type: 'array',
+                    items: { type: 'object', properties: itemSchema }
+                }
+            }], 'out');
+        }
+
+        if (outputType === 'file') {
+            return context.sendJson([{ label: 'File ID', value: 'fileId' }], 'out');
+        }
+    }
+};
+
+/**
+ * @param {array} array
+ * @returns {string}
+ */
+const toCsv = (array) => {
+    const headers = Object.keys(array[0]);
+
+    return [
+        headers.join(','),
+
+        ...array.map(items => {
+            return Object.values(items).map(property => {
+                if (typeof property === 'object') {
+                    return JSON.stringify(property);
+                }
+                return property;
+            }).join(',');
+        })
+
+    ].join('\n');
+};
