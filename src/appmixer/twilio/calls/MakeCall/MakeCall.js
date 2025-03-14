dafa63f6d1abe21f1d79d11597977c586b85f0d1
Sayam Nasir <sayam.nasir53@gmail.com>
2025-03-14 15:49:32 +0500

    some improvements

--- src/appmixer/twilio/calls/MakeCall/MakeCall.js
+++ src/appmixer/twilio/calls/MakeCall/MakeCall.js
@@ -6,20 +6,19 @@ module.exports = {
     async receive(context) {
 
         if (context.messages.webhook) {
-            return context.sendJson(context.messages.webhook.content.data, 'call');
+            await context.sendJson(context.messages.webhook.content.data, 'call');
+            return context.response();
         }
 
         const { accountSID, authenticationToken } = context.auth;
         const { from, to } = context.messages.in.content;
         const client = twilio(accountSID, authenticationToken);
-
+        const endpoint = context.getWebhookUrl();
         await client.calls.create({
-            statusCallback: context.getWebhookUrl(),
-            statusCallbackEvent: [
-                'completed', 'busy',
-                'failed', 'no-answer', 'canceled'
-            ],
-            url: context.getWebhookUrl(),
+            statusCallback: endpoint,
+            statusCallbackEvent: ["initiated", "answered"],
+            statusCallbackMethod: "POST",
+            url: endpoint,
             to,
             from
         });
