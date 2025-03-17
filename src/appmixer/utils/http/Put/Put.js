fd2bfebb6fc9789b46dcb1fbce80475258d4e63b
Sayam Nasir <sayam.nasir53@gmail.com>
2025-03-17 15:25:25 +0500

    http.Utils.POST, PUT (patch) Added support for binary file upload (#420)

--- src/appmixer/utils/http/Put/Put.js
+++ src/appmixer/utils/http/Put/Put.js
@@ -1,5 +1,5 @@
-'use strict';
 const request = require('../http-commons');
+const FormData = require('form-data');
 
 /**
  * This component is used to send HTTP request
@@ -7,11 +7,92 @@ const request = require('../http-commons');
  */
 module.exports = {
 
-    receive(context) {
+    async receive(context) {
+
+        const { url, headers, bodyType, fileId } = context.messages.in.content;
+
+        if (bodyType === 'binary') {
+            return await sendBinaryData(context, url, headers, fileId);
+        }
+
+        if (bodyType === 'form-data') {
+
+            let headersParsed;
+            try {
+                headersParsed = typeof headers === 'string' ? JSON.parse(headers) : headers;
+            } catch (error) {
+                throw new context.CancelError('Message property "headers" parse error. ' + error.message);
+            }
+
+            return await sendFormData(context, url, headersParsed);
+        }
 
         return request('PUT', context.messages.in.content)
-            .then(response  => {
-                return context.sendJson(response , 'response');
+            .then(response => {
+                return context.sendJson(response, 'response');
             });
     }
 };
+
+const sendBinaryData = async (context, url, headers, binaryFileId) => {
+    try {
+        const fileStream = await context.getFileReadStream(binaryFileId);
+        const fileInfo = await context.getFileInfo(binaryFileId);
+
+        const response = await context.httpRequest({
+            method: 'put',
+            url,
+            data: fileStream,
+            headers: {
+                'Content-Type': 'application/octet-stream',
+                'Content-Length': fileInfo.length,
+                ...headers
+            }
+        });
+
+        return await context.sendJson(response.data, 'response');
+    } catch (error) {
+        throw new Error(`Failed to send binary data: ${error.message}`);
+    }
+};
+
+const sendFormData = async (context, url, headers) => {
+
+    const { bodyFormData } = context.messages.in.content;
+    const variablesArray = bodyFormData?.ADD || [];
+
+    const formData = new FormData();
+
+    for (const variable of variablesArray) {
+        const { name, type } = variable;
+        const value = variable[type];
+
+        switch (type) {
+            case 'filepicker':
+                const fileInfo = await context.getFileInfo(value);
+                const stream = await context.getFileReadStream(value);
+                formData.append(name, stream, {
+                    filename: fileInfo.filename,
+                    contentType: fileInfo.contentType,
+                    knownLength: fileInfo.length
+                });
+                break;
+            case 'text':
+            case 'date-time':
+            case 'number':
+            case 'toggle':
+                formData.append(name, value);
+                break;
+        }
+    }
+
+    const response = await context.httpRequest({
+        method: 'put',
+        url,
+        data: formData,
+        headers: { ...formData.getHeaders(), ...headers }
+    });
+
+    return await context.sendJson(response.data, 'response');
+};
+
