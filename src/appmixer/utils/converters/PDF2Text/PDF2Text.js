650915e5925556c194c0a6412f4129f205eb4be4
David Durman <daviddurman@gmail.com>
2025-03-25 15:29:45 +0100

    limit pdf file size to 10MB

--- src/appmixer/utils/converters/PDF2Text/PDF2Text.js
+++ src/appmixer/utils/converters/PDF2Text/PDF2Text.js
@@ -3,12 +3,17 @@ const converters = require('../converters');
 const path = require('path');
 const { Readable } = require('stream');
 
+const MAX_PDF_FILE_SIZE = 1024 * 1024 * 10; // 10MB
+
 module.exports = {
 
     async receive(context) {
 
         const { fileId, outputTextContent } = context.messages.in.content;
         const fileInfo = await context.getFileInfo(fileId);
+        if (fileInfo.length > MAX_PDF_FILE_SIZE) {
+            throw new context.CancelError(`PDF file is too large. Max file size is ${MAX_PDF_FILE_SIZE / 1024 / 1024}MB.`);
+        }
         const newFileName = path.parse(fileInfo.filename).name + '.txt';
         const textContent = await converters.pdfToText(context, fileId);
         const savedFile = await context.saveFileStream(newFileName, Readable.from(textContent));
