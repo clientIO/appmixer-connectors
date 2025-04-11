4adc587246bfffba1ee968797b3f63cb088b59f6
vladimir talas <vladimir.talas@gmail.com>
2025-04-11 15:24:30 +0200

    update tests

--- src/appmixer/wiz/core/UploadScan/UploadScan.js
+++ src/appmixer/wiz/core/UploadScan/UploadScan.js
@@ -7,7 +7,7 @@ module.exports = {
 
         const { scheduleValue } = context.properties;
         if (scheduleValue) {
-            return this.scheduleDrain(context, { previousDate: null });
+            return this.scheduleDrain(context);
         }
     },
 
@@ -25,21 +25,24 @@ module.exports = {
             if (context.messages.timeout) {
                 await this.scheduleDrain(context);
                 if (documents.length > 0) {
-                    await this.sendDocuments(context, documents);
+                    const integrationId = await context.stateGet('integrationId');
+                    const filename = await context.stateGet('filename');
+                    await this.sendDocuments(context, { documents, filename, integrationId });
                 }
                 return;
             }
 
-            const { document } = context.messages.in.content;
+            const { document, filename, integrationId } = context.messages.in.content;
             documents.push(document);
             context.log({ step: 'documents in state:', count: documents.length });
             await context.stateSet('documents', documents);
+            await context.stateSet('filename', filename);
+            await context.stateSet('integrationId', integrationId);
 
             if (threshold && documents.length >= threshold) {
-                return this.sendDocuments(context, documents);
-            }
-            if (!threshold && !scheduleValue) {
-                return this.sendDocuments(context, documents);
+                await this.sendDocuments(context, { documents, filename, integrationId });
+            } else if (!threshold && !scheduleValue) {
+                await this.sendDocuments(context, { documents, filename, integrationId });
             }
 
         } finally {
@@ -66,17 +69,13 @@ module.exports = {
         await context.setTimeout({}, diff);
     },
 
-    async sendDocuments(context, dataSources) {
-
-        await context.stateUnset('documents');
-
-        const { filename, integrationId } = context.messages.in.content;
+    async sendDocuments(context, { documents, filename, integrationId }) {
 
         const { url, systemActivityId } = await lib.requestUpload(context, { filename });
 
         const fileContent = {
             integrationId,
-            dataSources: dataSources
+            dataSources: documents
         };
 
         context.log({ step: 'filecontent', fileContent });
@@ -84,6 +83,7 @@ module.exports = {
         const systemActivity = await lib.getStatus(context, systemActivityId);
         context.log({ step: 'systemActivity', systemActivity });
 
+        await context.stateUnset('documents');
         // throw error if the system activity is not valid.
         lib.validateUploadStatus(context, { systemActivity });
 
