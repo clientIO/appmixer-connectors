5fd8f002a0d653d2044843593123922c8359f98f
Zbynek Pelunek <zbynekpelunek@gmail.com>
2025-04-25 09:03:33 +0200

    Microsoft.Sharepoint (major) new components - data ingestion flow (#461)
    * Microsoft.Sharepoint (major) new components - data ingestion flow
    
    * standup feedback changes
    
    * lint fix
    
    * first batch of tests for FindFilesOrFolders
    
    * FindFilesOrFolders now correctly returns images when parentPath is set
    
    * eslint fix
    
    * added onedrive-api package to root, FindFilesOrFolders pagination + other coderabbit suggestions
    
    * remove console logs
    
    * add back onedrive-api package to root package.json
    
    * outputFileData option for DownloadFile
    
    * package lock fix hopefully
    
    * add request-promise to root
    
    * if accessToken is empty, no sharepoint tests will run

---
+++ src/appmixer/microsoft/sharepoint/FindFilesOrFolders/FindFilesOrFolders.js
@@ -0,0 +1,75 @@
+'use strict';
+
+const { makeRequest } = require('../common');
+const commons = require('../../microsoft-commons');
+
+const MAX_SIZE = 10000;
+
+module.exports = {
+
+    async receive(context) {
+        const { driveId, parentPath, fileTypesRestriction, outputType } = context.messages.in.content;
+        let { q } = context.messages.in.content;
+
+        if (!q) q = '';
+        const encodedQuery = encodeURIComponent(q);
+
+        let nextLink;
+
+        // Different URL is only used, because using /root:/<parentPath>:/search is unreliable
+        // For example, it doesn't return all images even when searched for them using the query
+        if (parentPath) {
+            nextLink = `https://graph.microsoft.com/v1.0/drives/${driveId}/root:/${parentPath}:/delta?$top=${MAX_SIZE}`;
+        } else {
+            nextLink = `https://graph.microsoft.com/v1.0/drives/${driveId}/root/search(q='${encodedQuery}')?$top=${MAX_SIZE}`;
+        }
+
+        let filesAndFolder = [];
+        do {
+            const response = await makeRequest(
+                {
+                    url: nextLink,
+                    method: 'GET',
+                    body: null
+                },
+                context
+            );
+
+            nextLink = response['@odata.nextLink'];
+            filesAndFolder = filesAndFolder.concat(response.value);
+
+        } while (nextLink);
+
+        if (parentPath && q) {
+            filesAndFolder = filesAndFolder.filter((f) => f.name.includes(q));
+        }
+
+        if (filesAndFolder.length === 0) {
+            return context.sendJson({}, 'notFound');
+        }
+
+        if (fileTypesRestriction?.length > 0) {
+            const allowedFilesOrFolders = filesAndFolder.filter(file =>
+                fileTypesRestriction.some(typeRestriction =>
+                    file.file?.mimeType.startsWith(typeRestriction)
+                )
+            );
+            if (allowedFilesOrFolders.length === 0) {
+                return context.sendJson({}, 'notFound');
+            }
+
+            return await commons.sendArrayOutput({
+                context,
+                outputType,
+                records: allowedFilesOrFolders
+            });
+        } else {
+            return await commons.sendArrayOutput({
+                context,
+                outputType,
+                records: filesAndFolder
+            });
+        }
+
+    }
+};
