1cbfbc31b3cfd8b1bd67ed821a696c201779fd3a
Sayam Nasir <sayam.nasir53@gmail.com>
2025-04-29 18:41:53 +0500

    updates

--- src/appmixer/nexl/core/GetLists/GetLists.js
+++ src/appmixer/nexl/core/GetLists/GetLists.js
@@ -1,14 +1,50 @@
 'use strict';
-
 const lib = require('../../lib');
 
+const query = `query {
+    lists(
+      filter: { listStatus: ACTIVE },
+      page: 1,
+      perPage: 500
+    ) {
+      entries {
+        id
+        name
+        contactsCount
+        createdAt
+        updatedAt
+        creator {
+          id
+          email
+        }
+      }
+      pageInfo {
+        totalEntries
+        totalPages
+      }
+    }
+  }`;
+
 module.exports = {
+    async receive(context) {
+        const response = await lib.makeApiCall({
+            context,
+            method: 'POST',
+            data: { query }
+        });
 
-    async start(context) {
-        const query = 'query {lists(filter: { listStatus: ACTIVE },page: 1,perPage: 500) {entries {id name contacts Count creator {id email}}}}';
-        context.log({ query });
-        const data = lib.apiRequest(context, query);
+        if (response.data.errors) {
+            throw new context.CancelError(response.data.data.errors);
+        }
+        console.log(response.data.data);
+        const lists = response.data.data.lists.entries;
+        return context.sendJson(lists, 'out');
+    },
 
-        return context.sendJson(data.data, 'out');
+    listsToSelectArray(lists) {
+
+        return lists.map(list => {
+            return { label: `${list.name}`, value: `${list.id}` };
+        });
     }
 };
--- src/appmixer/nexl/lib.js
+++ src/appmixer/nexl/lib.js
@@ -1,21 +1,17 @@
 'use strict';
 
 module.exports = {
-  async apiRequest(context, query) {
-    context.log({ context });
-    context.log({ query });
-    const response = await context.httpRequest({
-      method: 'POST',
-      url: `https://${context.auth.regionPrefix}.nexl.cloud/graphql`,
-      headers: {
-        Authorization: `Bearer ${context.auth.apiKey}`,
-        'Content-Type': 'application/json',
-      },
-      data: {
-        query,
-      },
+  async makeApiCall({ context, method = 'GET', data }) {
+
+    const url = `https://${context.auth.regionPrefix}.nexl.cloud/api/graphql`;
+    return context.httpRequest({
+        method,
+        url,
+        headers: {
+            'content-type': 'application/json',
+            'authorization': `Bearer ${context.auth.apiKey}`
+        },
+        data
     });
-    context.log({ step: 'response', response: response.data });
-    return response.data;
-  },
+}
 };
