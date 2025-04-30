dbfc0440b27c362c4411fa5bcdee7076abaa2d99
Sayam Nasir <sayam.nasir53@gmail.com>
2025-04-30 14:11:18 +0500

    lint2

--- src/appmixer/nexl/lib.js
+++ src/appmixer/nexl/lib.js
@@ -2,15 +2,15 @@
 
 module.exports = {
     async makeApiCall({ context, method = 'GET', data }) {
-      const url = `https://${context.auth.regionPrefix}.nexl.cloud/api/graphql`;
-      return context.httpRequest({
-        method,
-        url,
-        headers: {
-          'content-type': 'application/json',
-          'authorization': `Bearer ${context.auth.apiKey}`
-        },
-        data
-      });
+        const url = `https://${context.auth.regionPrefix}.nexl.cloud/api/graphql`;
+        return context.httpRequest({
+            method,
+            url,
+            headers: {
+                'content-type': 'application/json',
+                authorization: `Bearer ${context.auth.apiKey}`
+            },
+            data
+        });
     }
-  };
+};
