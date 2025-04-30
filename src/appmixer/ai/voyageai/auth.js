da3525836bd7f8c2c23e7bfb4e9d2157edf77d1f
Jiří Hofman <jiri.hofman@gmail.com>
2025-04-30 13:04:32 +0200

    Update API key tooltip and modify key display format in VoyageAI auth module

--- src/appmixer/ai/voyageai/auth.js
+++ src/appmixer/ai/voyageai/auth.js
@@ -11,20 +11,21 @@ module.exports = {
             apiKey: {
                 type: 'text',
                 name: 'API Key',
-                tooltip: 'Log into your service account and find <i>API Keys</i> page.'
+                tooltip: 'Log into your dashboard at https://dashboard.voyageai.com/api-keys to create an API key.'
             }
         },
 
         requestProfileInfo(context) {
             const apiKey = context.apiKey;
             return {
-                key: apiKey.substr(0, 13) + '...' + apiKey.substr(apiKey.length - 9)
+                key: apiKey.substr(0, 3) + '...' + apiKey.substr(apiKey.length - 4)
             };
         },
 
         accountNameFromProfileInfo: 'key',
 
         validate: (context) => {
+            // TODO: Add validation for the API key once VoyageAI provides a way to validate it.
             if (!context.apiKey) {
                 throw new Error('API Key is required.');
             }
