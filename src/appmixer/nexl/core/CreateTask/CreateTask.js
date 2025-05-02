6d3b6231d9b9a4289ab11ac075b915d22e95d6c3
Sayam Nasir <sayam.nasir53@gmail.com>
2025-05-02 15:59:40 +0500

    eslint1

--- src/appmixer/nexl/core/CreateTask/CreateTask.js
+++ src/appmixer/nexl/core/CreateTask/CreateTask.js
@@ -3,52 +3,52 @@
 const lib = require('../../lib');
 
 const mutation = `mutation CreateTask(
-  $assignedToUid: ID,
-  $attributes: CreateTaskAttributes!
+    $assignedToUid: ID,
+    $attributes: CreateTaskAttributes!
 ) {
-  createTask(
-    assignedToUid: $assignedToUid,
-    attributes: $attributes
-  ) {
-    failReasons
-    record {
-      id
-      title
-      description {
-        plainText
-      }
-      assignedTo {
-        id
-        email
-      }
-      createdAt
-      updatedAt
+    createTask(
+        assignedToUid: $assignedToUid,
+        attributes: $attributes
+    ) {
+        failReasons
+        record {
+            id
+            title
+            description {
+                plainText
+            }
+            assignedTo {
+                id
+                email
+            }
+            createdAt
+            updatedAt
+        }
     }
-  }
 }`;
 
 module.exports = {
-  async receive(context) {
-    const { assignedToUid, title, description } = context.messages.in.content;
+    async receive(context) {
+        const { assignedToUid, title, description } = context.messages.in.content;
 
-    const variables = {
-      assignedToUid,
-      attributes: {
-        title,
-        description
-      }
-    };
+        const variables = {
+            assignedToUid,
+            attributes: {
+                title,
+                description
+            }
+        };
 
-    const { data } = await lib.makeApiCall({
-      context,
-      method: 'POST',
-      data: { query: mutation, variables }
-    });
+        const { data } = await lib.makeApiCall({
+            context,
+            method: 'POST',
+            data: { query: mutation, variables }
+        });
 
-    if (data.errors) {
-      throw new context.CancelError(data.errors);
+        if (data.errors) {
+            throw new context.CancelError(data.errors);
+        }
+
+        return context.sendJson(data.data.createTask.record, 'out');
     }
-
-    return context.sendJson(data.data.createTask.record, 'out');
-  }
 };
