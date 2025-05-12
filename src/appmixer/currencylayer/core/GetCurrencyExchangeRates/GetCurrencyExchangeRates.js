a82c5fc619e088a2f992018554784f4fbc58b7e5
Sayam Nasir <sayam.nasir53@gmail.com>
2025-05-12 17:40:22 +0500

    Currencylayer (patch) adding cache and retries to avoid limits
    Added cache in source endpoint and added retries in GetCurrencyExchangeRates and GetSingleCurrencyRate as it is possible that multiple components are run at the same time and the api will give 106

--- src/appmixer/currencylayer/core/GetCurrencyExchangeRates/GetCurrencyExchangeRates.js
+++ src/appmixer/currencylayer/core/GetCurrencyExchangeRates/GetCurrencyExchangeRates.js
@@ -11,7 +11,32 @@ module.exports = {
         const params = {
             source: context.messages.in.content.source
         };
-        const data = await fetchData(context, 'live', params);
+        let retryCount = 0;
+        let data;
+        // Retry up to 3 times
+        while (retryCount < 3) {
+            data = await fetchData(context, 'live', params);
+            // If the API call is successful, break out of the loop
+            if (data.success) {
+                break;
+            }
+            // If the error code is 106, wait for 5 seconds before retrying
+            if (data.error?.code === 106) {
+                retryCount++;
+                if (retryCount < 3) {
+                    await new Promise(resolve => setTimeout(resolve, 5000)); // 5 seconds delay
+                }
+            }
+            else {
+                // If the error is not 106, stop retrying and throw an error
+                throw new context.CancelError(`API returned error: ${data.error?.code || 'unknown error'}`);
+            }
+        }
+        // If we retried 3 times and still got no success, throw error
+        if (!data.success) {
+            throw new context.CancelError('API failed after 3 retries');
+        }
+        // If the API call is successful, process the data
         const result = data.quotes;
         const exchangeRates = Object.keys(result).map(currencies=>{
             return {
