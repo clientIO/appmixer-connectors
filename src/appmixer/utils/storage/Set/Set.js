852e6bf0de1efa4a36596b6ff1b473c96cb64ce9
Sayam Nasir <sayam.nasir53@gmail.com>
2025-05-15 15:08:26 +0500

    Set-many

--- src/appmixer/utils/storage/Set/Set.js
+++ src/appmixer/utils/storage/Set/Set.js
@@ -1,14 +1,25 @@
 'use strict';
 
 module.exports = {
-
     async receive(context) {
+        const { keyValuePairs, storeId } = context.messages.in.content;
 
-        const { key, value, storeId } = context.messages.in.content;
-        const setData = await context.store.set(storeId, key, value);
+        let results = [];
 
-        return context.sendJson(Object.assign(setData, {
-            newValue: value
-        }), 'out');
+        // Iterate through each key-value pair and store them
+        for (const pair of keyValuePairs.AND) {
+            const { key, value } = pair;
+
+            // Set the data in the store
+            const setData = await context.store.set(storeId, key, value);
+
+            // Collect the response data for each key-value pair
+            results.push(Object.assign(setData, {
+                key,
+                newValue: value
+            }));
+        }
+        // Return the collected results
+        return context.sendJson({ results }, 'out');
     }
 };
