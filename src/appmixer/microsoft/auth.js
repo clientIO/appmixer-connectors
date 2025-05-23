bf25c485d2d2a79acde729ddc117a1bf3c08a224
Zbynek Pelunek <zbynekpelunek@gmail.com>
2025-05-23 13:24:01 +0200

    finalize + update bundle everywhere

--- src/appmixer/microsoft/auth.js
+++ src/appmixer/microsoft/auth.js
@@ -1,5 +1,13 @@
 'use strict';
 
+const hasTenant = function(context) {
+    const microsoftTenant = context.config?.microsoftTenant;
+    if (microsoftTenant === undefined) {
+        return false;
+    }
+    return microsoftTenant === true || typeof microsoftTenant === 'string' && microsoftTenant.toLowerCase() === 'true' || false;
+};
+
 module.exports = {
 
     type: 'oauth2',
@@ -10,39 +18,93 @@ module.exports = {
 
         scopeDelimiter: ' ',
 
-        pre: {
-            tenant: {
-                type: 'text',
-                name: 'Tenant',
-                tooltip: 'Specify the Tenant. Valid values are common, organizations, consumers, and tenant identifiers.'
+        pre: function(context) {
+            if (hasTenant(context)) {
+                return {
+                    microsoftTentant: {
+                        type: 'text',
+                        name: 'Tenant',
+                        tooltip: 'Specify the Tenant. Valid values are common, organizations, consumers, and tenant identifiers.'
+                    }
+                };
             }
+
+            return;
         },
 
         authUrl: context => {
-            const microsoftTenantId = context.tenant ||
-                context.config.tenant ||
-                process.env.TENANT ||
-                'common';
+            const microsoftTenantId = hasTenant(context) ? '{{microsoftTentant}}' : 'common';
 
-            return `https://login.microsoftonline.com/${microsoftTenantId}/oauth2/v2.0/authorize`;
+            return `https://login.microsoftonline.com/${microsoftTenantId}/oauth2/v2.0/authorize`
+                + `?client_id=${encodeURIComponent(context.clientId)}`
+                + `&response_type=code`
+                + `&redirect_uri=${context.callbackUrl}`
+                + `&state=${context.ticket}`
+                + `&scope=${context.scope.join(module.exports.definition.scopeDelimiter)}`;
         },
 
-        requestAccessToken: context => {
-            const microsoftTenantId = context.tenant ||
-                context.config.tenant ||
-                process.env.TENANT ||
-                'common';
+        requestAccessToken: async (context) => {
+            const microsoftTenantId = hasTenant(context) ? context.microsoftTentant : 'common';
 
-            return `https://login.microsoftonline.com/${microsoftTenantId}/oauth2/v2.0/token`;
+            const { authorizationCode } = context;
+            const url = `https://login.microsoftonline.com/${microsoftTenantId}/oauth2/v2.0/token`;
+            const clientId = encodeURIComponent(context.clientId);
+            const clientSecret = encodeURIComponent(context.clientSecret);
+            const tokenRequestData = `client_id=${clientId}`
+                + `&client_secret=${clientSecret}`
+                + `&code=${encodeURIComponent(authorizationCode)}`
+                + '&grant_type=authorization_code'
+                + `&redirect_uri=${encodeURIComponent(context.callbackUrl)}`;
+
+            const options = {
+                url,
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                },
+                data: tokenRequestData
+            };
+            const { data } = await context.httpRequest(options);
+
+            const accessTokenExpDate = new Date();
+            accessTokenExpDate.setTime(accessTokenExpDate.getTime() + (data.expires_in * 1000));
+
+            return {
+                accessToken: data.access_token,
+                refreshToken: data.refresh_token,
+                accessTokenExpDate
+            };
         },
 
-        refreshAccessToken: context => {
-            const microsoftTenantId = context.tenant ||
-                context.config.tenant ||
-                process.env.TENANT ||
-                'common';
+        refreshAccessToken: async (context) => {
+            const microsoftTenantId = hasTenant(context) ? context.microsoftTentant : 'common';
 
-            return `https://login.microsoftonline.com/${microsoftTenantId}/oauth2/v2.0/token`;
+            const url = `https://login.microsoftonline.com/${microsoftTenantId}/oauth2/v2.0/token`;
+            const clientId = encodeURIComponent(context.clientId);
+            const clientSecret = encodeURIComponent(context.clientSecret);
+            const tokenRequestData = `client_id=${clientId}`
+                + `&client_secret=${clientSecret}`
+                + '&grant_type=refresh_token'
+                + `&refresh_token=${context.refreshToken}`;
+
+            const options = {
+                url,
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                },
+                data: tokenRequestData
+            };
+            const { data } = await context.httpRequest(options);
+
+            const accessTokenExpDate = new Date();
+            accessTokenExpDate.setTime(accessTokenExpDate.getTime() + (data.expires_in * 1000));
+
+            return {
+                accessToken: data.access_token,
+                accessTokenExpDate,
+                refreshToken: data.refresh_token
+            };
         },
 
         accountNameFromProfileInfo: (context) => {
