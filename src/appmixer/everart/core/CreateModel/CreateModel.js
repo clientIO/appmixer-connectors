d64a09c690353d545ace33259e4d7d8b6192c9b7
vladimir talas <vladimir.talas@gmail.com>
2025-06-09 14:21:53 +0200

    update

--- src/appmixer/everart/core/CreateModel/CreateModel.js
+++ src/appmixer/everart/core/CreateModel/CreateModel.js
@@ -1,18 +1,16 @@
 module.exports = {
     async receive(context) {
-        const { name, subject, image_urls, image_upload_tokens } = context.messages.in.content;
+        const { name, subject, image_urls: imageUrls } = context.messages.in.content;
 
         const payload = {
             name,
             subject
         };
 
-        if (image_urls.AND.length > 0) {
-            payload.image_urls = image_urls.AND.map(item => item['image_urls_item']);
-        } else if (image_upload_tokens.AND.length > 0) {
-            payload.image_upload_tokens = image_upload_tokens.AND.map(item => item['image_upload_tokens_item']);
+        if (imageUrls.AND.length > 0) {
+            payload.image_urls = imageUrls.AND.map(item => item['image_urls_item']);
         } else {
-            return context.CancelError('At least one of "Image Upload Tokens" or "Image Urls" must be provided.');
+            return context.CancelError('"Image Urls" must be provided.');
         }
 
         // https://www.everart.ai/api/docs/#/Models/post_models_post
