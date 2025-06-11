7273b3a919a972c667d7c95614658286cc75d83f
Petr Olivka <petr.olivka@gmail.com>
2025-06-11 20:19:20 +0200

    FetchSheets component stub

---
+++ src/appmixer/hubsync/grid/FetchSheets/FetchSheets.js
@@ -0,0 +1,35 @@
+"use strict";
+module.exports = {
+    async receive(context) {
+        const {auth} = context;
+        const {workspaceId, databaseId} = context.messages.in.content;
+
+        const url = `${auth.baseUrl}/api/datagrid/workspaces/${workspaceId}/databases/${databaseId}/sheets`;
+
+        try {
+            const response = await context.httpRequest({
+                method: "GET",
+                url,
+                headers: {
+                    "X-Api-Key": auth.apiKey,
+                    "X-Tenant": auth.tenant,
+                    "Content-Type": "application/json"
+                }
+            });
+            //console.log('Fetched workspaces:', response.data);
+            return context.sendJson(this.sheetsToSelectArray(response.data), 'sheets');
+        } catch (error) {
+            throw new Error(`Failed to fetch sheets: ${error.message}`);
+        }
+    },
+
+    sheetsToSelectArray(sheets) {
+        if (sheets && Array.isArray(sheets)) {
+            return sheets.map(sheet => ({
+                label: sheet.title,
+                value: sheet.id
+            }));
+        }
+        return [];
+    }
+};
