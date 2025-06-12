9480bf3c03af056edf01c24ab1fbd84f5859a625
Zbynek Pelunek <zbynekpelunek@gmail.com>
2025-06-12 20:52:30 +0200

    SendMessages components update

--- src/appmixer/line/core/SendPushMessage/SendPushMessage.js
+++ src/appmixer/line/core/SendPushMessage/SendPushMessage.js
@@ -1,15 +1,27 @@
-
-const lib = require('../../lib.generated');
 module.exports = {
     async receive(context) {
-        const { to, messages } = context.messages.in.content;
+        const { to, messages, notificationDisabled } = context.messages.in.content;
+
+        const messagesArr = messages.ADD.map((message) => {
+            return {
+                type: message.type,
+                text: message.text,
+                packageId: message.packageId,
+                stickerId: message.stickerId
+            };
+        });
 
         // https://developers.line.biz/en/reference/messaging-api/#send-push-message
         const { data } = await context.httpRequest({
             method: 'POST',
             url: 'https://api.line.me/v2/bot/message/push',
             headers: {
-                'Authorization': `Bearer ${context.auth.apiToken}`
+                'Authorization': `Bearer ${context.auth.channelAccessToken}`
+            },
+            data: {
+                to,
+                notificationDisabled,
+                messages: messagesArr
             }
         });
 
--- src/appmixer/line/core/SendReplyMessage/SendReplyMessage.js
+++ src/appmixer/line/core/SendReplyMessage/SendReplyMessage.js
@@ -1,15 +1,27 @@
-
-const lib = require('../../lib.generated');
 module.exports = {
     async receive(context) {
-        const { replyToken, messages } = context.messages.in.content;
+        const { replyToken, messages, notificationDisabled } = context.messages.in.content;
+
+        const messagesArr = messages.ADD.map((message) => {
+            return {
+                type: message.type,
+                text: message.text,
+                packageId: message.packageId,
+                stickerId: message.stickerId
+            };
+        });
 
         // https://developers.line.biz/en/reference/messaging-api/#send-reply-message
         const { data } = await context.httpRequest({
             method: 'POST',
             url: 'https://api.line.me/v2/bot/message/reply',
             headers: {
-                'Authorization': `Bearer ${context.auth.apiToken}`
+                'Authorization': `Bearer ${context.auth.channelAccessToken}`
+            },
+            data: {
+                replyToken,
+                notificationDisabled,
+                messages: messagesArr
             }
         });
 
