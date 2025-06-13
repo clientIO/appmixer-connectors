eb5ba7dbe36e48f7bfda526c768608465eb69050
vladimir talas <lades@client.io>
2025-06-13 09:41:40 +0200

    utils.chat (minor) new ChatSetProgressStatus component; Chat widget a… (#577)
    * utils.chat (minor) new ChatSetProgressStatus component; Chat widget adjusted to show progress
    
    * fix expireAt date
    
    * chat now uses SSE to get messages and progress status in real-time
    
    * add check for when the flow is offline
    
    * connect to event stream; support deltas
    
    * send initial SSE comment to kickstart stream; implement bettter delta handling
    
    ---------
    
    Co-authored-by: David Durman <daviddurman@gmail.com>

--- src/appmixer/utils/chat/lib.js
+++ src/appmixer/utils/chat/lib.js
@@ -1,4 +1,6 @@
 const { URL } = require('url');
+const fs = require('fs').promises;
+const Redis = require('ioredis');
 
 const page = (baseUrl, endpoint) => {
     return `
@@ -13,6 +15,7 @@ const page = (baseUrl, endpoint) => {
         <script type="text/javascript" src="${baseUrl}/plugins/appmixer/utils/chat/assets/chat.lib.js"></script>
         <script type="text/javascript">
            const ENDPOINT = '${endpoint}';
+           const BASE_URL = '${baseUrl}';
         </script>
         <script type="text/javascript" src="${baseUrl}/plugins/appmixer/utils/chat/assets/chat.main.js"></script>
         </body>
@@ -23,8 +26,49 @@ const page = (baseUrl, endpoint) => {
 module.exports = {
 
     generateWebUI: function(endpoint) {
+
         const parsedUrl = new URL(endpoint);
         const baseUrl = `${parsedUrl.protocol}//${parsedUrl.hostname}`;
         return page(baseUrl, endpoint);
+    },
+
+    connectRedis: async function() {
+
+        const connection = {
+            uri: process.env.REDIS_URI,
+            mode: process.env.REDIS_MODE || 'standalone',
+            sentinels: process.env.REDIS_SENTINELS,
+            sentinelMasterName: process.env.REDIS_SENTINEL_MASTER_NAME,
+            sentinelRedisPassword: process.env.REDIS_SENTINEL_PASSWORD,
+            enableTLSForSentinelMode: process.env.REDIS_SENTINEL_ENABLE_TLS,
+            caPath: process.env.REDIS_CA_PATH,
+            useSSL: process.env.REDIS_USE_SSL === 'true' || parseInt(process.env.REDIS_USE_SSL) > 0
+        };
+
+        const options = {};
+        if (connection.useSSL) {
+            options.tls = {
+                ca: connection.caPath ? await fs.readFile(connection.caPath) : undefined
+            };
+        }
+
+        let client;
+
+        if (connection.mode === 'replica' && connection.sentinels) {
+
+            const sentinelsArray = connection.sentinels.split(',');
+
+            client = new Redis({
+                sentinels: sentinelsArray,
+                name: connection.sentinelMasterName,
+                ...(connection.sentinelRedisPaswword ? { password: connection.sentinelRedisPaswword } : {}),
+                ...(connection.enableTLSForSentinelMode ?
+                    { enableTLSForSentinelMode: connection.enableTLSForSentinelMode } : {})
+            });
+        } else {
+            client = connection.uri ? new Redis(connection.uri, options) : new Redis();
+        }
+
+        return client;
     }
 };
