e25d4d0a0a37bd6f37e4ea049a087f1518660418
Sayam Nasir <sayam.nasir53@gmail.com>
2025-06-13 17:04:06 +0500

    new

--- src/appmixer/pinterest/core/DeletePin/DeletePin.js
+++ src/appmixer/pinterest/core/DeletePin/DeletePin.js
@@ -3,13 +3,13 @@
 module.exports = {
     async receive(context) {
 
-        const { boardId } = context.messages.in.content;
+        const { pinId } = context.messages.in.content;
 
         // https://developers.pinterest.com/docs/api/v5/boards/#delete-board
         /* eslint-disable no-unused-vars */
         const { data } = await context.httpRequest({
             method: 'DELETE',
-            url: `https://api.pinterest.com/v5/boards/${boardId}`,
+            url: `https://api.pinterest.com/v5/pins/${pinId}`,
             headers: {
                 'Authorization': `Bearer ${context.auth.accessToken}`
             }
