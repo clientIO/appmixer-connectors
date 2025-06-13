1ab40fbccaa84051974b403f56f76e43ecd33735
vladimir talas <vladimir.talas@gmail.com>
2025-06-13 14:57:47 +0200

    apigee

--- src/appmixer/apigee/core/ListKeyValueMaps/ListKeyValueMaps.js
+++ src/appmixer/apigee/core/ListKeyValueMaps/ListKeyValueMaps.js
@@ -1,22 +1,9 @@
 'use strict';
 
-const lib = require('../../lib.generated');
-const schema = {
-    'name': { 'type': 'string', 'title': 'Name' },
-    'encrypted': { 'type': 'boolean', 'title': 'Encrypted' }
-};
-
 module.exports = {
     async receive(context) {
 
-        const { org, env, outputType } = context.messages.in.content;
-
-        if (context.properties.generateOutputPortOptions) {
-            return lib.getOutputPortOptions(context, outputType, schema, {
-                label: 'Key Value Maps',
-                value: 'keyValueMaps'
-            });
-        }
+        const { org, env } = context.messages.in.content;
 
         // https://cloud.google.com/apigee/docs/reference/apis/apigee/rest/v1/organizations.environments.keyvaluemaps/list
         const { data } = await context.httpRequest({
@@ -27,19 +14,6 @@ module.exports = {
             }
         });
 
-        // The API returns an array of keyvaluemap names, we need to transform them
-        const records = data.keyValueMaps ? data.keyValueMaps.map(kvmName => {
-            return {
-                name: kvmName,
-                encrypted: false // Default value, as the list API doesn't return this info
-            };
-        }) : [];
-
-        return lib.sendArrayOutput({
-            context,
-            records,
-            outputType,
-            outputPortName: 'out'
-        });
+        return context.sendJson(data, 'out');
     }
 };
