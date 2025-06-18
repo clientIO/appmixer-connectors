449b8f11d6033348bdaef8a376c59e721dcea045
Petr Olivka <petr.olivka@gmail.com>
2025-06-18 15:03:49 +0200

    Updateitem refactor

--- src/appmixer/hubsync/grid/UpdateItem/UpdateItem.js
+++ src/appmixer/hubsync/grid/UpdateItem/UpdateItem.js
@@ -3,35 +3,35 @@
 module.exports = {
     async receive(context) {
         const { auth } = context;
-        const { workspaceId, databaseId, sheetId, itemId } = context.properties;
-        const { fields } = context.messages.in.content;
-        
+        const { workspaceId, databaseId, sheetId } = context.properties;
+        const { fields, itemId } = context.messages.in.content;
+
         let fieldsObject = {};
         try {
             fieldsObject = JSON.parse(fields);
         } catch (error) {
-            throw new context.CancelError('Invalid fields JSON');
+            throw new context.CancelError("Invalid fields JSON");
+        }
+
+        if (!itemId) {
+            throw new context.CancelError("Item ID must be provided.");
         }
 
         const url = `${auth.baseUrl}/api/datagrid/workspaces/${workspaceId}/databases/${databaseId}/sheets/${sheetId}/items/${itemId}`;
 
-        try {
-            const response = await context.httpRequest({
-                method: "PUT",
-                url,
-                headers: {
-                    "X-Api-Key": auth.apiKey,
-                    "X-Tenant": auth.tenant,
-                    "Content-Type": "application/json"
-                },
-                data: {
-                    "itemId": itemId,
-                    "fields": fields
-                }
-            });
-            return context.sendJson(response.data, 'updatedItem');
-        } catch (error) {
-            throw new Error(`Failed to update item: ${error.message}`);
-        }
-    }
+        const response = await context.httpRequest({
+            method: "PUT",
+            url,
+            headers: {
+                "X-Api-Key": auth.apiKey,
+                "X-Tenant": auth.tenant,
+                "Content-Type": "application/json",
+                Accept: "application/json",
+            },
+            data: {
+                fields: fieldsObject,
+            },
+        });
+        return context.sendJson(response.data, "updatedItem");
+    },
 };
