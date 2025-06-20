b52feabf3de22c4cf5626202270729b2357cb45d
Sayam Nasir <sayam.nasir53@gmail.com>
2025-06-20 19:50:20 +0500

    Linkein (patch) Version update (#584)
    * Linkein (patch) Version update
    
    older version has been sunset
    
    * bundle bumo
    
    * lint1
    
    * typo

--- src/appmixer/linkedin/constants.js
+++ src/appmixer/linkedin/constants.js
@@ -1,5 +1,5 @@
 const VERSION_PATH = 'rest';
 const BASE_URL = 'https://api.linkedin.com/';
-const VERSION_HEADER = '202403';
+const VERSION_HEADER = '202506';
 
 module.exports = { VERSION_PATH, BASE_URL, VERSION_HEADER };
