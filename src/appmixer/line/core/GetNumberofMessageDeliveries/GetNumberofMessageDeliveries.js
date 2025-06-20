027dee00f10c0ddaa7b4eb38c2364d831aae4327
Zbynek Pelunek <zbynekpelunek@gmail.com>
2025-06-20 09:34:37 +0200

    LINE (new) (#531)
    * LINE (new)
    
    * update
    
    * SendMessages components update
    
    * LeaveGroupOrRoom component
    
    * finalize components
    
    * coderabbit feedback
    
    * remove zip file
    
    * update
    
    * feedback updates
    
    * coderabbit feedback

---
+++ src/appmixer/line/core/GetNumberofMessageDeliveries/GetNumberofMessageDeliveries.js
@@ -0,0 +1,19 @@
+module.exports = {
+    async receive(context) {
+        const { date } = context.messages.in.content;
+
+        // https://developers.line.biz/en/reference/messaging-api/#get-number-of-message-deliveries
+        const { data } = await context.httpRequest({
+            method: 'GET',
+            url: 'https://api.line.me/v2/bot/insight/message/delivery',
+            headers: {
+                'Authorization': `Bearer ${context.auth.channelAccessToken}`
+            },
+            params: {
+                date
+            }
+        });
+
+        return context.sendJson(data, 'out');
+    }
+};
