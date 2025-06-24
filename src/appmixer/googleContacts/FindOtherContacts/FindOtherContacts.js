23c83d4ab9c8fd5c73eced22bbb932f2890fa6a9
Zbynek Pelunek <zbynekpelunek@gmail.com>
2025-06-24 13:45:52 +0200

    GoogleContacts (new) - moved from Google repo (#592)

---
+++ src/appmixer/googleContacts/FindOtherContacts/FindOtherContacts.js
@@ -0,0 +1,43 @@
+const lib = require('../lib.generated');
+const { personSchema } = require('./../schemas');
+
+module.exports = {
+    async receive(context) {
+        const { outputType } = context.messages.in.content;
+
+        if (context.properties.generateOutputPortOptions) {
+            return lib.getOutputPortOptions(context, outputType, personSchema, { label: 'Other Contacts', value: 'result' });
+        }
+
+        const { data } = await context.httpRequest({
+            method: 'GET',
+            url: 'https://people.googleapis.com/v1/otherContacts',
+            headers: {
+                'Authorization': `Bearer ${context.auth.accessToken}`
+            },
+            params: {
+                readMask: 'emailAddresses,metadata,names,phoneNumbers,photos'
+            }
+        });
+
+        if (!Array.isArray(data.otherContacts) || !data.otherContacts.length) {
+            return context.sendJson({}, 'notFound');
+        }
+
+        const records = data.otherContacts.map((contact) => {
+            return {
+                id: contact.resourceName.split('/')[1],
+                etag: contact.etag,
+                updateTime: contact.metadata.sources[0].updateTime,
+                displayName: contact.names[0].displayName,
+                givenName: contact.names[0].givenName,
+                displayNameLastFirst: contact.names[0].displayNameLastFirst,
+                unstructuredName: contact.names[0].unstructuredName,
+                photoUrl: contact.photos[0].url,
+                memberships: contact.memberships
+            };
+        });
+
+        return lib.sendArrayOutput({ context, records, outputType });
+    }
+};
